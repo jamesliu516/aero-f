dff83c3b584af526bc668492aeb35f551feba395
arallu <arallu@stanford.edu>
2008-06-06 17:56:59 -0700

    Modifications concerning the input file for Volumes-, PorousMedia- and MultiFluidData- objects (in class IoData). Input file was modified so that each element with a volume id could have either its own Equation of State or its porous properties. This implied some changes in the class IoData as well as in the way the 2-phase flow field was initialized and how BC were applied. All these changes should not affect single-phase flows, except when a porous media is specified (only the way to specify its data and how these data are stored were changed in IoData; during the construction of the FemEquationTerm class, the relevant information is extracted from IoData to store it in the same data structure before).

---
+++ Elem.C
@@ -0,0 +1,227 @@
+#include <Elem.h>
+#include <Face.h>
+
+#include <FemEquationTerm.h>
+#include <MacroCell.h>
+#include <VMSLESTerm.h>
+#include <DynamicVMSTerm.h>
+#include <SmagorinskyLESTerm.h>
+#include <WaleLESTerm.h>
+#include <DynamicLESTerm.h>
+#include <GenMatrix.h>
+#include <math.h>
+#include <GeoState.h>
+
+
+//------------------------------------------------------------------------------
+//--------------functions in ElemSet class
+//------------------------------------------------------------------------------
+
+template<int dim>
+void ElemSet::computeGalerkinTerm(FemEquationTerm *fet, GeoState &geoState, 
+				  SVec<double,3> &X, SVec<double,dim> &V, 
+				  SVec<double,dim> &R)
+{
+
+  Vec<double> &d2wall = geoState.getDistanceToWall();
+
+  for (int i=0; i<numElems; ++i)
+    elems[i]->computeGalerkinTerm(fet, X, d2wall, V, R);
+
+}
+
+//------------------------------------------------------------------------------
+
+// Included
+template<int dim>
+void ElemSet::computeDerivativeOfGalerkinTerm(FemEquationTerm *fet, GeoState &geoState,
+				 SVec<double,3> &X, SVec<double,3> &dX, SVec<double,dim> &V, SVec<double,dim> &dV,
+				 double dMach, SVec<double,dim> &dR)
+{
+
+  Vec<double> &d2wall = geoState.getDistanceToWall();
+
+  for (int i=0; i<numElems; ++i)
+    elems[i]->computeDerivativeOfGalerkinTerm(fet, X, dX, d2wall, V, dV, dMach, dR);
+
+}
+
+//------------------------------------------------------------------------------
+
+template<int dim>
+void ElemSet::computeMBarAndM(DynamicVMSTerm *dvmst,
+			      SVec<double,dim> **VBar,
+			      SVec<double,1> **volRatio,
+			      SVec<double,3> &X,
+			      SVec<double,dim> &V,
+			      SVec<double,dim> &MBar,
+			      SVec<double,dim> &M)
+{
+
+  for (int i=0; i<numElems; ++i)
+   elems[i]->computeMBarAndM(dvmst, VBar, volRatio, X, V, MBar, M);
+
+}
+
+//------------------------------------------------------------------------------
+
+template<int dim>
+void ElemSet::computeDynamicVMSTerm(DynamicVMSTerm *dvmst,
+				    SVec<double,dim> **VBar,
+				    SVec<double,3> &X,
+				    SVec<double,dim> &V, SVec<double,dim> &S,
+				    Vec<double> &CsDelSq, Vec<double> &PrT,
+				    Vec<double> *Cs, Vec<double> &Delta)
+{
+
+  for (int i=0; i<numElems; ++i)
+    elems[i]->computeDynamicVMSTerm(dvmst, VBar, X, V, S, CsDelSq, PrT, Cs, Delta);
+
+}
+
+//------------------------------------------------------------------------------
+
+template<int dim>
+void ElemSet::computeVMSLESTerm(VMSLESTerm *vmst,
+				SVec<double,dim> &VBar,
+				SVec<double,3> &X,
+				SVec<double,dim> &V,
+				SVec<double,dim> &Sigma)
+                                                                                                                          
+{
+                                                                                                                          
+  for (int i=0; i<numElems; ++i)
+    elems[i]->computeVMSLESTerm(vmst, VBar, X, V, Sigma);
+                                                                                                                          
+}
+
+//------------------------------------------------------------------------------
+
+template<int dim>
+void ElemSet::computeSmagorinskyLESTerm(SmagorinskyLESTerm *smag, SVec<double,3> &X,
+					SVec<double,dim> &V, SVec<double,dim> &R)
+
+{
+  for (int i=0; i<numElems; ++i)
+    elems[i]->computeSmagorinskyLESTerm(smag, X, V, R);
+}
+
+//------------------------------------------------------------------------------
+
+template<int dim>
+void ElemSet::computeWaleLESTerm(WaleLESTerm *wale, SVec<double,3> &X,
+				SVec<double,dim> &V, SVec<double,dim> &R)
+
+{
+  for (int i=0; i<numElems; ++i)
+    elems[i]->computeWaleLESTerm(wale, X, V, R);
+}
+
+//------------------------------------------------------------------------------
+
+template<int dim>
+void ElemSet::computeDynamicLESTerm(DynamicLESTerm *dles, SVec<double,2> &Cs, Vec<double> &VolSum,
+				    SVec<double,3> &X, SVec<double,dim> &V, SVec<double,dim> &R)
+
+{
+
+ for (int i=0; i<numElems; ++i)
+    elems[i]->computeDynamicLESTerm(dles, Cs, VolSum, X, V, R);
+
+}
+
+//------------------------------------------------------------------------------
+template<int dim, class Scalar, int neq>
+void ElemSet::computeJacobianGalerkinTerm(FemEquationTerm *fet, GeoState &geoState, 
+					  SVec<double,3> &X, Vec<double> &ctrlVol,
+					  SVec<double,dim> &V, GenMat<Scalar,neq> &A)
+{
+
+  Vec<double> &d2wall = geoState.getDistanceToWall();
+
+  for (int i=0; i<numElems; ++i)
+    elems[i]->computeJacobianGalerkinTerm(fet, X, ctrlVol, d2wall, V, A);
+
+}
+
+//------------------------------------------------------------------------------
+
+template<int dim>
+void ElemSet::computeTestFilterAvgs(SVec<double,dim> &VCap, SVec<double,16> &Mom_Test,
+				    SVec<double,6> &Eng_Test, SVec<double,3> &X, SVec<double,dim> &V, double gam,
+				    double R)
+
+{
+
+ for (int i=0; i<numElems; ++i)
+   elems[i]->computeP1Avg(VCap, Mom_Test, Eng_Test, X, V, gam, R);
+
+}
+
+//------------------------------------------------------------------------------
+
+template<int dim>
+void ElemSet::computeCsValues(SVec<double,dim> &VCap, SVec<double,16> &Mom_Test,
+			      SVec<double,6> &Eng_Test, SVec<double,2> &Cs, 
+			      Vec<double> &VolSum, SVec<double,3> &X, double gam, double R)
+
+{
+
+ for (int i=0; i<numElems; ++i)
+   elems[i]->computeCsValues(VCap, Mom_Test, Eng_Test, Cs, VolSum, X, gam, R);
+
+}
+
+//------------------------------------------------------------------------------
+// Level Set Reinitialization functions
+
+template<int dim>
+void ElemSet::computePsiResidual(SVec<double,3> &X,Vec<double> &Phi,SVec<double,dim> &Psi,
+				SVec<double,dim> &ddx, SVec<double,dim> &ddy,
+			   	SVec<double,dim> &ddz, Vec<int> &Tag,
+                                Vec<double> &w,Vec<double> &beta, SVec<double,dim> &PsiRes,
+ 				int typeTracking)
+{
+
+  for (int i=0; i<numElems; i++)
+    elems[i]->computePsiResidual(X,Phi,Psi,ddx,ddy,ddz,Tag,w,beta,PsiRes,typeTracking);
+
+}
+
+//------------------------------------------------------------------------------
+template<int dim>
+void ElemSet::computeDistanceCloseNodes(Vec<int> &Tag, SVec<double,3> &X,
+                                       SVec<double,dim> &ddx, SVec<double,dim> &ddy,
+                                       SVec<double,dim> &ddz,
+                                       Vec<double> &Phi,SVec<double,dim> &Psi)
+{
+
+  for (int i=0; i<numElems; i++)
+    elems[i]->computeDistanceCloseNodes(Tag,X,ddx,ddy,ddz,Phi,Psi);
+
+}
+//------------------------------------------------------------------------------
+template<int dim>
+void ElemSet::recomputeDistanceCloseNodes(Vec<int> &Tag, SVec<double,3> &X,
+                                       SVec<double,dim> &ddx, SVec<double,dim> &ddy,
+                                       SVec<double,dim> &ddz,
+                                       Vec<double> &Phi,SVec<double,dim> &Psi)
+{
+
+  for (int i=0; i<numElems; i++)
+    elems[i]->recomputeDistanceCloseNodes(Tag,X,ddx,ddy,ddz,Phi,Psi);
+
+}
+//------------------------------------------------------------------------------
+template<int dim>
+void ElemSet::computeDistanceLevelNodes(Vec<int> &Tag, int level,
+                                       SVec<double,3> &X, SVec<double,dim> &Psi, Vec<double> &Phi)
+{
+
+  for (int i=0; i<numElems; i++)
+    elems[i]->computeDistanceLevelNodes(Tag,level,X,Psi,Phi);
+
+}
+// End of Level Set Reinitialization functions
+//-------------------------------------------------------------------------------
+
---
+++ FluxFcnDescLiquidInLiquid.h
@@ -0,0 +1,179 @@
+#ifndef _FLUX_FCN_DESC_LIQUID_IN_LIQUID_H_
+#define _FLUX_FCN_DESC_LIQUID_IN_LIQUID_H_
+
+#include <FluxFcnDesc.h>
+#include <VarFcnDesc.h>
+
+class IoData;
+//------------------------------------------------------------------------------
+
+class FluxFcnLiquidInLiquidFDJacRoeEuler3D : public FluxFcnFDJacRoeEuler3D {
+
+public:
+
+  FluxFcnLiquidInLiquidFDJacRoeEuler3D(double gg, double br, double K1, double cm, double sr, int pr, IoData &ioData, Type tp = CONSERVATIVE) :
+    FluxFcnFDJacRoeEuler3D(gg, br, K1, cm, sr, pr, new VarFcnLiquidInLiquidEuler3D(ioData), tp) {}
+  ~FluxFcnLiquidInLiquidFDJacRoeEuler3D() {}
+
+  void compute(double, double, double *, double, double *, double *, double *, int);
+
+// Included (MB)
+  void computeDerivative(double ire, double dIre, double *n, double *dn, double nv, double dnv, double *vl, double *dvl, double *vr, double *dvr, double dmach, double *f, double *df, int fl=1) {}
+  void computeDerivative(double ire, double dIre, double *n, double *dn, double nv, double dnv, double *v, double *ub, double *dub, double *f, double *df, int fl=1) {}
+
+};
+
+//------------------------------------------------------------------------------
+
+class FluxFcnLiquidInLiquidApprJacRoeEuler3D : public FluxFcnApprJacRoeEuler3D {
+
+public:
+
+  FluxFcnLiquidInLiquidApprJacRoeEuler3D(int rs, double gg, double br, double K1, double cm, double sr, int pr, IoData &ioData, Type tp = CONSERVATIVE) : 
+    FluxFcnApprJacRoeEuler3D(rs, gg, br, K1, cm, sr, pr, new VarFcnLiquidInLiquidEuler3D(ioData), tp) {}
+  ~FluxFcnLiquidInLiquidApprJacRoeEuler3D() {}
+
+  void compute(double, double, double *, double, double *, double *, double *, int);
+  void computeJacobians(double, double *, double, double *, double *, double *, double *, int);
+
+// Included (MB)
+  void computeDerivative(double ire, double dIre, double *n, double *dn, double nv, double dnv, double *vl, double *dvl, double *vr, double *dvr, double dmach, double *f, double *df, int fl=1) {}
+  void computeDerivative(double ire, double dIre, double *n, double *dn, double nv, double dnv, double *v, double *ub, double *dub, double *f, double *df, int fl=1) {}
+
+};
+
+//------------------------------------------------------------------------------
+
+class FluxFcnLiquidInLiquidExactJacRoeEuler3D : public FluxFcnExactJacRoeEuler3D {
+
+public:
+
+  FluxFcnLiquidInLiquidExactJacRoeEuler3D(double gg, IoData &ioData, Type tp = CONSERVATIVE) : 
+    FluxFcnExactJacRoeEuler3D(gg, new VarFcnLiquidInLiquidEuler3D(ioData), tp) {}
+  ~FluxFcnLiquidInLiquidExactJacRoeEuler3D() {}
+
+  void compute(double, double, double *, double, double *, double *, double *, int);
+  void computeJacobians(double, double *, double, double *, double *, double *, double *, int);
+
+// Included (MB)
+  void computeDerivative(double ire, double dIre, double *n, double *dn, double nv, double dnv, double *vl, double *dvl, double *vr, double *dvr, double dmach, double *f, double *df, int fl=1) {}
+  void computeDerivative(double ire, double dIre, double *n, double *dn, double nv, double dnv, double *v, double *ub, double *dub, double *f, double *df, int fl=1) {}
+
+};
+
+//------------------------------------------------------------------------------
+
+//NO VAN LEER 
+
+//------------------------------------------------------------------------------
+
+class FluxFcnLiquidInLiquidWallEuler3D : public FluxFcnWallEuler3D {
+
+public:
+  FluxFcnLiquidInLiquidWallEuler3D(IoData &ioData, Type tp=CONSERVATIVE) :
+    FluxFcnWallEuler3D(new VarFcnLiquidInLiquidEuler3D(ioData), tp) {}
+  ~FluxFcnLiquidInLiquidWallEuler3D() {}
+
+  void compute(double, double, double *, double, double *, double *, double *, int);
+
+// Included (MB)
+  void computeDerivative(double ire, double dIre, double *n, double *dn, double nv, double dnv, double *vl, double *dvl, double *vr, double *dvr, double dmach, double *f, double *df, int fl=1) {}
+  void computeDerivative(double ire, double dIre, double *n, double *dn, double nv, double dnv, double *v, double *ub, double *dub, double *f, double *df, int fl=1) {}
+
+};
+
+//------------------------------------------------------------------------------
+
+class FluxFcnLiquidInLiquidGhidagliaEuler3D : public FluxFcnGhidagliaEuler3D {
+
+public:
+
+  FluxFcnLiquidInLiquidGhidagliaEuler3D(IoData &ioData, Type tp = CONSERVATIVE) :
+    FluxFcnGhidagliaEuler3D(new VarFcnLiquidInLiquidEuler3D(ioData), tp) {}
+  ~FluxFcnLiquidInLiquidGhidagliaEuler3D() {}
+
+  void compute(double, double, double *, double, double *, double *, double *, int);
+
+// Included (MB)
+  void computeDerivative(double ire, double dIre, double *n, double *dn, double nv, double dnv, double *vl, double *dvl, double *vr, double *dvr, double dmach, double *f, double *df, int fl=1) {}
+  void computeDerivative(double ire, double dIre, double *n, double *dn, double nv, double dnv, double *v, double *ub, double *dub, double *f, double *df, int fl=1) {}
+
+};
+
+//------------------------------------------------------------------------------
+
+class FluxFcnLiquidInLiquidInflowEuler3D : public FluxFcnInflowEuler3D {
+
+public:
+
+  FluxFcnLiquidInLiquidInflowEuler3D(IoData &ioData, Type tp = CONSERVATIVE) :
+    FluxFcnInflowEuler3D(new VarFcnLiquidInLiquidEuler3D(ioData), tp) {}
+  ~FluxFcnLiquidInLiquidInflowEuler3D() {}
+
+  void compute(double, double, double *, double, double *, double *, double *, int);
+
+// Included (MB)
+  void computeDerivative(double ire, double dIre, double *n, double *dn, double nv, double dnv, double *vl, double *dvl, double *vr, double *dvr, double dmach, double *f, double *df, int fl=1) {}
+  void computeDerivative(double ire, double dIre, double *n, double *dn, double nv, double dnv, double *v, double *ub, double *dub, double *f, double *df, int fl=1) {}
+
+};
+
+//------------------------------------------------------------------------------
+
+class FluxFcnLiquidInLiquidOutflowEuler3D : public FluxFcnOutflowEuler3D {
+
+public:
+
+  FluxFcnLiquidInLiquidOutflowEuler3D(IoData &ioData, Type tp = CONSERVATIVE) :
+    FluxFcnOutflowEuler3D(new VarFcnLiquidInLiquidEuler3D(ioData), tp) {}
+  ~FluxFcnLiquidInLiquidOutflowEuler3D() {}
+
+  void compute(double, double, double *, double, double *, double *, double *, int);
+
+// Included (MB)
+  void computeDerivative(double ire, double dIre, double *n, double *dn, double nv, double dnv, double *vl, double *dvl, double *vr, double *dvr, double dmach, double *f, double *df, int fl=1) {}
+  void computeDerivative(double ire, double dIre, double *n, double *dn, double nv, double dnv, double *v, double *ub, double *dub, double *f, double *df, int fl=1) {}
+
+};
+
+//------------------------------------------------------------------------------
+
+class FluxFcnLiquidInLiquidInternalInflowEuler3D : public FluxFcnInternalInflowEuler3D {
+
+public:
+
+  FluxFcnLiquidInLiquidInternalInflowEuler3D(IoData &ioData, Type tp = CONSERVATIVE) : 
+    FluxFcnInternalInflowEuler3D(new VarFcnLiquidInLiquidEuler3D(ioData), tp) {}
+  ~FluxFcnLiquidInLiquidInternalInflowEuler3D() {}
+
+  void compute(double, double, double *, double, double *, double *, double *, int);
+  void computeJacobian(double, double *, double, double *, double *, double *, int);
+
+// Included (MB)
+  void computeDerivative(double ire, double dIre, double *n, double *dn, double nv, double dnv, double *vl, double *dvl, double *vr, double *dvr, double dmach, double *f, double *df, int fl=1) {}
+  void computeDerivative(double ire, double dIre, double *n, double *dn, double nv, double dnv, double *v, double *ub, double *dub, double *f, double *df, int fl=1) {}
+
+};
+
+//------------------------------------------------------------------------------
+
+class FluxFcnLiquidInLiquidInternalOutflowEuler3D : public FluxFcnInternalOutflowEuler3D {
+
+public:
+
+  FluxFcnLiquidInLiquidInternalOutflowEuler3D(IoData &ioData, Type tp = CONSERVATIVE) : 
+    FluxFcnInternalOutflowEuler3D(new VarFcnLiquidInLiquidEuler3D(ioData), tp) {}
+  ~FluxFcnLiquidInLiquidInternalOutflowEuler3D() {}
+
+  void compute(double, double, double *, double, double *, double *, double *, int);
+  void computeJacobian(double, double *, double, double *, double *, double *, int);
+
+// Included (MB)
+  void computeDerivative(double ire, double dIre, double *n, double *dn, double nv, double dnv, double *vl, double *dvl, double *vr, double *dvr, double dmach, double *f, double *df, int fl=1) {}
+  void computeDerivative(double ire, double dIre, double *n, double *dn, double nv, double dnv, double *v, double *ub, double *dub, double *f, double *df, int fl=1) {}
+
+};
+
+//------------------------------------------------------------------------------
+
+#endif
