d5025a80a3a39b1df6d66b1670be3378ed9c4462
brogniez <brogniez@nivation.stanford.edu>
2009-01-30 16:10:11 -0800

    RK2 on moving grid A new algorithm for the RK2 on moving grid has been implemented. It is now conservative. Computing a term of Volume change is not required anymore.

---
+++ DistExactRiemannSolver.h
@@ -0,0 +1,48 @@
+#ifndef _DIST_EXACT_RIEMANN_H_
+#define _DIST_EXACT_RIEMANN_H_
+
+
+class IoData;
+class Domain;
+
+template<class Scalar, int dim> class DistSVec;
+template<int dim> class ExactRiemannSolver;
+//------------------------------------------------------------------------------
+
+template<int dim>
+class DistExactRiemannSolver {
+
+  // updatePhase indicates if we should use riemannupdate (cf below)
+  bool updatePhase;
+  bool firstpass;
+  int numLocSub;
+
+  DistSVec<double,dim> *riemannupdate;
+  DistVec<double> *weight;
+  // riemannupdate is used only when using GFMPAR
+  // it stores the future value at each cell if
+  // that cell changes phases between two time steps
+
+  ExactRiemannSolver<dim> **subExactRiemannSolver;
+
+
+public:
+  DistExactRiemannSolver(IoData &iod, Domain *dom);
+  ~DistExactRiemannSolver();
+
+  bool DoUpdatePhase() { return updatePhase; }
+  DistSVec<double,dim> *getRiemannUpdate() const { return riemannupdate; }
+  DistVec<double> *getRiemannWeight() const { return weight; }
+
+  ExactRiemannSolver<dim> &operator() (int i) 
+    const { return *subExactRiemannSolver[i]; }
+
+};
+//------------------------------------------------------------------------------
+
+#ifdef TEMPLATE_FIX
+#include <DistExactRiemannSolver.C>
+#endif
+
+#endif
+
