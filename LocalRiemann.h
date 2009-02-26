36b0ca4afce36c896d18b02d79494fc41163735a
arallu <arallu@stanford.edu>
2009-02-25 16:45:55 -0800

    change in parser (IoData.C) to setup the FailSafe object

---
+++ LocalRiemann.h
@@ -0,0 +1,184 @@
+#ifndef _LOCAL_RIEMANN_H
+#define _LOCAL_RIEMANN_H
+
+#include <LinkF77.h>
+
+class VarFcn;
+
+extern "C" {
+  void F77NAME(eriemanngw) (const double&, const double&, const double&,
+                            const double&, const double&, const double&,
+                            const double&, const double&, const double &,
+                            const double &, const double&, const double&,
+                            const double &, const double&);
+  void F77NAME(eriemanngg) (const double&, const double&, const double&,
+                            const double&, const double&, const double&,
+                            const double&, const double&, const double &,
+                            const double &, const double&, const double&,
+                            const double &, const double&);
+  void F77NAME(eriemannww) (const double&, const double&, const double&,
+                            const double&, const double&, const double&,
+                            const double&, const double&, const double&,
+                            const double&, const double&, const double&,
+                            const double&, const double&, const double&,
+                            const double&);
+};
+//------------------------------------------------------------------------------
+
+class LocalRiemann {
+//this is a virtual class.
+//only subclasses will be created:
+//    -LocalRiemannGfmpGasGas
+//    -LocalRiemannGfmpTaitTait
+//    -LocalRiemannGfmparGasGas
+//    -LocalRiemannGfmparGasTait
+//    -LocalRiemannGfmparTaitTait
+
+public:
+  LocalRiemann() {}
+  ~LocalRiemann() {}
+
+  virtual void computeRiemannSolution(double *Vi, double *Vj,
+                            double Phii, double Phij, double *nphi, VarFcn *vf,
+                            int &epsi, int &epsj, double *Wi, double *Wj,
+                            double *rupdatei, double *rupdatej,
+                            double &weighti, double &weightj, int it){}
+
+  virtual void eriemann(double rhol, double ul, double pl, 
+                        double rhor, double ur, double pr, 
+                        double &pi, double &ui,  
+                        double &rhoil, double &rhoir,
+                        VarFcn *vf){}
+
+protected:
+  virtual void solve2x2System(double *mat, double *rhs, double *res);
+
+  void riemannInvariant(VarFcn *vf, double phi,
+                   double v1, double u1, double p1, 
+                   double v,  double &u, double &p, 
+                   double &du, double &dp);
+  void shockJWL(VarFcn *vf, double phi, double omega,
+                double omp1oom, double frho, double frhoi, 
+                double frhopi,
+                double v, double u, double p, double vi,
+                double &ui, double &pi, double &dui, double &dpi);
+
+  void shockGAS(double phi, double gamogam1,
+                double pref,
+                double v, double u, double p, 
+                double vi, double &ui, double &pi,
+                double &dui, double &dpi);
+  void riemannInvariantGAS(VarFcn *vf, double phi,
+                   double gam, double gam1, double pref, double c1, 
+                   double v1, double u1, double p1,
+                   double v, double &u, double &p,
+                   double &du, double &dp);
+};
+
+//----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+inline
+void LocalRiemann::solve2x2System(double *mat, double *rhs, double *res)
+{
+  double determinant = mat[0]*mat[3]-mat[1]*mat[2];
+  double eps = 1.0e-15;
+  double norm = 1.0;
+  if(fabs(determinant)>eps*norm){
+    res[0] = ( mat[3]*rhs[0]-mat[1]*rhs[1])/determinant;
+    res[1] = (-mat[2]*rhs[0]+mat[0]*rhs[1])/determinant;
+  }else{
+    fprintf(stdout, "zero-determinant\n");
+    res[0] = 0.0;
+    res[1] = 0.0;
+  }
+
+}
+
+//----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+inline
+void LocalRiemann::riemannInvariant(VarFcn *vf, double phi,
+                   double v1, double u1, double p1, 
+                   double v,  double &u, double &p, 
+                   double &du, double &dp){
+//compute integrals using ODEs. Integrand are not approximated.
+//integrate 
+  //fprintf(stdout, "riemannInvariant\n");
+  int N = 1000;
+  double t  = v1;
+  double dt = (v-v1)/N;
+  double c;
+  u = u1; p = p1;
+  double V[5] = { 1.0/v1, u1, 0.0, 0.0, p1 };
+  bool continueCondition = true;
+  //fprintf(stdout, "begin loop\n");
+  while(continueCondition){
+    c = vf->computeSoundSpeed(V,phi);
+    u -= phi*c/t*dt;
+    p -= c*c/(t*t)*dt;
+    t  += dt;
+    continueCondition = (t<v-dt/2.0);
+    V[0] = 1.0/t; V[1] = u; V[4] = p;
+  }
+  //fprintf(stdout, "end loop\n");
+  c = vf->computeSoundSpeed(V,phi);
+  du = -phi*c/v;
+  dp = -c*c/(v*v);
+  //fprintf(stdout, "end riemannInvariant\n");
+}
+inline
+void LocalRiemann::shockJWL(VarFcn *vf, double phi, double omega,
+                   double omp1oom, double frho, double frhoi, 
+                   double frhopi,
+                   double v, double u, double p, double vi,
+                   double &ui, double &pi, double &dui, double &dpi){
+//phi = -1 => left
+//phi = +1 => right
+  double den=omp1oom*vi-0.5*(vi+v);
+
+  pi = (omp1oom*v*p - 0.5*(vi+v)*p
+     + (frhoi*vi-frho*v)/omega
+       )/den;
+
+  ui = u + phi * sqrt(-(pi-p)*(vi - v));
+
+  dpi = ((frhoi - frhopi/vi)/omega
+      - 0.5*p - (omp1oom - 0.5)*pi)/den;
+
+  dui = -0.5*((vi-v)*dpi+pi-p)/(ui-u);
+
+}
+//---------------------------------------------------------------------------
+inline
+void LocalRiemann::riemannInvariantGAS(VarFcn *vf, double phi,
+                   double gam, double gam1, double pref, double c1,
+                   double v1, double u1, double p1,
+                   double v, double &u, double &p,
+                   double &du, double &dp){
+  p  = (p1+pref)*pow(v1/v,gam)-pref;
+  double V[5] = {1.0/v, u, 0.0, 0.0, p};
+  double c = vf->computeSoundSpeed(V,phi);
+  u  = u1 - phi*2.0/gam1*(c1 - c);
+
+  dp = -c*c/(v*v);
+  du = -phi*c/v;
+}
+inline
+void LocalRiemann::shockGAS(double phi, double gamogam1,
+                   double pref,
+                   double v, double u, double p, 
+                   double vi, double &ui, double &pi,
+                   double &dui, double &dpi){
+  double den=gamogam1*vi-0.5*(vi+v);
+
+  pi = (gamogam1*v - 0.5*(vi+v))*(p+pref)/den - pref;
+
+  ui = u + phi * sqrt(-(pi-p)*(vi - v));
+
+  dpi = ( -0.5*(p+pref) - (gamogam1 - 0.5)*(pi+pref))/den;
+
+  dui = -0.5*((vi-v)*dpi+pi-p)/(ui-u);
+}
+
+#endif
+
---
+++ MeshMotionHandler.h
@@ -0,0 +1,336 @@
+#ifndef _MESH_MOTION_HANDLER_H_
+#define _MESH_MOTION_HANDLER_H_
+
+#include <IoData.h>
+#include <DistVector.h>
+#include <Vector3D.h>
+
+class VarFcn;
+class MatchNodeSet;
+class Domain;
+class StructExc;
+class CorotSolver;
+class MeshMotionSolver;
+class MemoryPool;
+
+template<int dim> class PostOperator;
+
+//------------------------------------------------------------------------------
+
+class MeshMotionHandler {
+
+protected:
+
+  double tscale;
+  double oolscale;
+  double Wn;
+
+  DistSVec<double,3> F;
+  DistSVec<double,3> F0;
+  DistVec<double> Pin;
+  DistSVec<double,3> X0;
+  DistSVec<double,3> dX;
+
+  Domain *domain;
+  Communicator *com;
+
+public:
+
+  MeshMotionHandler(IoData &, Domain *);
+  ~MeshMotionHandler() {}
+
+  virtual double update(bool *, int, double, DistSVec<double,3> &, DistSVec<double,3> &) = 0;
+  virtual double updateStep1(bool *, int, double, DistSVec<double,3> &, DistSVec<double,3> &) {return 0.0;} 
+  virtual double updateStep2(bool *, int, double, DistSVec<double,3> &, DistSVec<double,3> &) {return 0.0;}
+
+  template<int dim>
+  void computeInterfaceWork(double, PostOperator<dim>*, DistSVec<double,3>&, 
+			    DistSVec<double,dim>&, DistSVec<double,3>&,
+			    DistSVec<double,dim>&, double*,
+			    DistVec<double> * = 0, DistVec<double> * = 0);
+
+  virtual int getAlgNum()  { return 0; }
+
+};
+
+//------------------------------------------------------------------------------
+
+class RigidMeshMotionHandler {
+
+protected: 
+  RigidMeshMotionData::Tag typeTag;
+  RigidMeshMotionData::LawType typeLaw;
+  double tscale;
+
+  Vec3D vin;
+  double cin;
+
+  Vec3D v;
+  Vec3D a;
+  Vec3D  deltaX;
+
+  int nvpts;
+  double *timeVpts;
+  Vec3D *velVpts;
+
+  DistSVec<double,3> Xr;
+
+public:
+
+  RigidMeshMotionHandler(IoData &, VarFcn *, double *, Domain *);
+  ~RigidMeshMotionHandler();
+
+  void setupVelocityPoints(IoData &);
+  void addRigidMotion(double, DistSVec<double,3> &, DistSVec<double,3> &, DistSVec<double,3> &, Communicator *com = 0);
+
+  DistSVec<double,3> &getRelativePositionVector(double, DistSVec<double,3> &);
+  DistSVec<double,3> &getFlightPositionVector(double, DistSVec<double,3> &);
+
+  const char* getTagName();
+  double getTagValue(double);
+  void updateMomentArm(Vec3D &x0);
+
+};
+
+//------------------------------------------------------------------------------
+
+class AccMeshMotionHandler : public MeshMotionHandler, 
+			     public RigidMeshMotionHandler {
+
+  double dt;
+
+public:
+
+  AccMeshMotionHandler(IoData &, VarFcn *, double *, Domain *);
+  ~AccMeshMotionHandler() {}
+
+  double update(bool *, int, double, DistSVec<double,3> &, DistSVec<double,3> &);
+  double updateStep1(bool *, int, double, DistSVec<double,3> &, DistSVec<double,3> &);
+  double updateStep2(bool *, int, double, DistSVec<double,3> &, DistSVec<double,3> &);
+
+};
+
+//------------------------------------------------------------------------------
+
+class AeroMeshMotionHandler : public MeshMotionHandler {
+
+protected:
+
+
+  AeroelasticData::Force forceComputation;
+  enum TimeIntegrator {IMPLICIT_FIRST_ORDER, IMPLICIT_SECOND_ORDER} timeIntegrator;
+  bool steady;
+  int it0;
+  double mppFactor;
+
+  DistSVec<double,3>* Fn;
+  DistSVec<double,3>* Fnp1;
+  DistSVec<double,3>* Favg;
+
+  StructExc *strExc;
+
+  MeshMotionSolver *mms;
+  char *posFile;
+
+public:
+
+  AeroMeshMotionHandler(IoData &, VarFcn *, double *, MatchNodeSet **, 
+			Domain *, MemoryPool *);
+  ~AeroMeshMotionHandler();
+
+  template<int dim>
+  void setup(int *, double *, PostOperator<dim>*, DistSVec<double,3>&, 
+             DistSVec<double,dim>&, DistVec<double> * = 0);
+
+  template<int dim>
+  void resetOutputToStructure(PostOperator<dim>*, DistSVec<double,3>&,
+                              DistSVec<double,dim>&, DistVec<double> * = 0);
+
+  template<int dim>
+  void updateOutputToStructure(double, double, PostOperator<dim>*,
+                               DistSVec<double,3>&, DistSVec<double,dim>&,
+                               DistVec<double> * = 0);
+
+  int getModalMotion(DistSVec<double,3> &);
+
+  virtual double update(bool *, int, double, DistSVec<double,3> &, DistSVec<double,3> &);
+  double updateStep1(bool *, int, double, DistSVec<double,3> &, DistSVec<double,3> &);
+  double updateStep2(bool *, int, double, DistSVec<double,3> &, DistSVec<double,3> &);
+  int getAlgNum(); 
+
+};
+
+//------------------------------------------------------------------------------
+
+class AccAeroMeshMotionHandler : public AeroMeshMotionHandler, 
+				 public RigidMeshMotionHandler {
+
+public:
+
+  AccAeroMeshMotionHandler(IoData &, VarFcn *, double *, MatchNodeSet **, 
+			   Domain *, MemoryPool *);
+  ~AccAeroMeshMotionHandler() {}
+
+  double update(bool *, int, double, DistSVec<double,3> &, DistSVec<double,3> &);
+  double updateStep1(bool *, int, double, DistSVec<double,3> &, DistSVec<double,3> &);
+  double updateStep2(bool *, int, double, DistSVec<double,3> &, DistSVec<double,3> &);
+
+};
+
+//------------------------------------------------------------------------------
+
+class DeformingMeshMotionHandler : public MeshMotionHandler {
+
+  double dt;
+  double omega;
+
+  DistSVec<double,3> *dXmax;
+
+  MeshMotionSolver *mms;
+
+public:
+
+  DeformingMeshMotionHandler(IoData &, Domain *);
+  ~DeformingMeshMotionHandler();
+
+  double update(bool *, int, double, DistSVec<double,3> &, DistSVec<double,3> &);
+  double updateStep1(bool *, int, double, DistSVec<double,3> &, DistSVec<double,3> &);
+  double updateStep2(bool *, int, double, DistSVec<double,3> &, DistSVec<double,3> &);
+
+  DistSVec<double,3> getModes();
+
+  void setup(DistSVec<double, 3> &X);
+
+};
+
+//------------------------------------------------------------------------------
+
+class PitchingMeshMotionHandler : public MeshMotionHandler {
+
+  double alpha_in;
+  double alpha_max;
+
+  double dt;
+  double omega;
+
+  double x1[3];
+  double x2[3];
+
+  double u, v, w;
+  double ix, iy, iz;
+
+  MeshMotionSolver *mms;
+
+public:
+
+  PitchingMeshMotionHandler(IoData &, Domain *);
+  ~PitchingMeshMotionHandler();
+
+  double update(bool *, int, double, DistSVec<double,3> &, DistSVec<double,3> &);
+  double updateStep1(bool *, int, double, DistSVec<double,3> &, DistSVec<double,3> &);
+  double updateStep2(bool *, int, double, DistSVec<double,3> &, DistSVec<double,3> &);
+
+  DistSVec<double,3> getModes();
+
+  void setup(DistSVec<double, 3> &X);
+
+};
+
+//------------------------------------------------------------------------------
+
+class HeavingMeshMotionHandler : public MeshMotionHandler {
+
+  double dt;
+  double omega;
+
+  double delta[3];
+
+  MeshMotionSolver *mms;
+
+public:
+
+  HeavingMeshMotionHandler(IoData &, Domain *);
+  ~HeavingMeshMotionHandler();
+
+  double update(bool *, int, double, DistSVec<double,3> &, DistSVec<double,3> &);
+  double updateStep1(bool *, int, double, DistSVec<double,3> &, DistSVec<double,3> &);
+  double updateStep2(bool *, int, double, DistSVec<double,3> &, DistSVec<double,3> &);
+
+  DistSVec<double,3> getModes();
+
+  void setup(DistSVec<double, 3> &X);
+
+};
+
+//------------------------------------------------------------------------------
+
+class AccForcedMeshMotionHandler : public DeformingMeshMotionHandler, 
+				   public RigidMeshMotionHandler {
+
+public:
+
+  AccForcedMeshMotionHandler(IoData &, VarFcn *, double *, Domain *);
+  ~AccForcedMeshMotionHandler() {}
+
+  double update(bool *, int, double, DistSVec<double,3> &, DistSVec<double,3> &);
+  double updateStep1(bool *, int, double, DistSVec<double,3> &, DistSVec<double,3> &);
+  double updateStep2(bool *, int, double, DistSVec<double,3> &, DistSVec<double,3> &);
+
+};
+
+//------------------------------------------------------------------------------
+
+class RigidRollMeshMotionHandler : public MeshMotionHandler {
+
+  double alpha_in;
+  double beta_in;
+
+  double dt;
+  double maxtime;
+
+  Vec3D xo;
+  Vec3D u;
+
+  MeshMotionSolver *mms;
+  
+private:
+
+  double computeRotationAngle(double);
+
+public:
+
+  RigidRollMeshMotionHandler(IoData &, double *, Domain *);
+  ~RigidRollMeshMotionHandler();
+
+  double update(bool *, int, double, DistSVec<double,3> &, DistSVec<double,3> &);
+  double updateStep2(bool *, int, double, DistSVec<double,3> &, DistSVec<double,3> &);
+
+};
+
+//------------------------------------------------------------------------------
+
+class RbmExtractor : public MeshMotionHandler {
+
+  char *name_in;
+  char *name_out1;
+  char *name_out2;
+
+  CorotSolver *cs;
+
+public:
+
+  RbmExtractor(IoData &, Domain *);
+  ~RbmExtractor();
+
+  double update(bool *, int, double, DistSVec<double,3> &, DistSVec<double,3> &);
+  double updateStep2(bool *, int, double, DistSVec<double,3> &, DistSVec<double,3> &);
+
+};
+
+//------------------------------------------------------------------------------
+
+#ifdef TEMPLATE_FIX
+#include <MeshMotionHandler.C>
+#endif
+
+#endif
