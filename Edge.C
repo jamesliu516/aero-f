244e078bab1e5dbd303fd12d60679333946f6d9f
kevinwgy <kevinwgy@su-ahpcrc.stanford.edu>
2009-04-04 11:02:37 -0700

    removed a printing

--- Edge.C
+++ Edge.C
@@ -526,7 +526,7 @@ int EdgeSet::computeFiniteVolumeTerm(ExactRiemannSolver<dim>& riemann, int* locT
   VarFcn *varFcn = fluxFcn[BC_INTERNAL]->getVarFcn();
   double length;
 
-  FILE *outFile = fopen("theEdges","w");
+  FILE *outFile = 0; //fopen("theEdges","w");
   //fprintf(stderr, "Working on the edges\n");
   int ierr=0;
   riemann.reset(it);
@@ -602,15 +602,15 @@ int EdgeSet::computeFiniteVolumeTerm(ExactRiemannSolver<dim>& riemann, int* locT
       if (iIsActive) {
         riemann.computeFSIRiemannSolution(Vi,res.normVel,res.gradPhi,varFcn,Wstar,j);
         for (int k=0; k<dim; k++) Wstarij[l][k] = Wstar[k]; //stores Wstar for later use.
-        double area = normal[l].norm(); //area of c.v. surface
+/*        double area = normal[l].norm(); //area of c.v. surface
         area *= normal[l]*res.gradPhi/normal[l].norm()*(-1.0); //projected to structure surface
         LSS.totalForce[0] += -Wstar[4]*res.gradPhi[0]*area;
         LSS.totalForce[1] += -Wstar[4]*res.gradPhi[1]*area;
         LSS.totalForce[2] += -Wstar[4]*res.gradPhi[2]*area;
-
-//        LSS.totalForce[0] += Wstar[4]*normal[l][0];
-//        LSS.totalForce[1] += Wstar[4]*normal[l][1];
-//        LSS.totalForce[2] += Wstar[4]*normal[l][2];
+*/
+        LSS.totalForce[0] += Wstar[4]*normal[l][0];
+        LSS.totalForce[1] += Wstar[4]*normal[l][1];
+        LSS.totalForce[2] += Wstar[4]*normal[l][2];
 
         fluxFcn[BC_INTERNAL]->compute(length, 0.0, normal[l], normalVel[l], Vi, Wstar, fluxi);
         for (int k=0; k<dim; k++) fluxes[i][k] += fluxi[k];
@@ -618,15 +618,15 @@ int EdgeSet::computeFiniteVolumeTerm(ExactRiemannSolver<dim>& riemann, int* locT
       if (jIsActive) {
         riemann.computeFSIRiemannSolution(Vj,res.normVel,res.gradPhi,varFcn,Wstar,i);
         for (int k=0; k<dim; k++) Wstarji[l][k] = Wstar[k];
-        double area = normal[l].norm(); //area of c.v. surface
+/*        double area = normal[l].norm(); //area of c.v. surface
         area *= normal[l]*res.gradPhi/normal[l].norm(); //projected to structure surface
         LSS.totalForce[0] += -Wstar[4]*res.gradPhi[0]*area;
         LSS.totalForce[1] += -Wstar[4]*res.gradPhi[1]*area;
         LSS.totalForce[2] += -Wstar[4]*res.gradPhi[2]*area;
-
-//        LSS.totalForce[0] += -Wstar[4]*normal[l][0];
-//        LSS.totalForce[1] += -Wstar[4]*normal[l][1];
-//        LSS.totalForce[2] += -Wstar[4]*normal[l][2];
+*/
+        LSS.totalForce[0] += -Wstar[4]*normal[l][0];
+        LSS.totalForce[1] += -Wstar[4]*normal[l][1];
+        LSS.totalForce[2] += -Wstar[4]*normal[l][2];
 
         fluxFcn[BC_INTERNAL]->compute(length, 0.0, normal[l], normalVel[l], Wstar, Vj, fluxj);
         for (int k=0; k<dim; k++)  fluxes[j][k] -= fluxj[k];
