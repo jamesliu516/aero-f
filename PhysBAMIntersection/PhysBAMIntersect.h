c1f3eff97f65b7ede314090085ef3d28ba9b175e
kevinwgy <kevinwgy@su-ahpcrc.stanford.edu>
2009-05-19 17:23:10 -0700

    more work done in PhysBAMIntersection

--- PhysBAMIntersection/PhysBAMIntersect.h
+++ PhysBAMIntersection/PhysBAMIntersect.h
@@ -33,6 +33,7 @@ class DistPhysBAMIntersector : public DistLevelSetStructure {
     Vec3D *solids_particle_list;
     Vec<Vec3D> *solidX;
     Vec3D *triNorms;
+    double *triSize;
     Communicator *com;
     PhysBAMIntersector **intersector;
 
@@ -75,17 +76,18 @@ class PhysBAMIntersector : public LevelSetStructure {
     int globIndex;
     int *locToGlobNodeMap;
     int *nodeMap;
-    std::map<int,IntersectionResult<double> > secondIntersection;
+
+    std::map<int,IntersectionResult<double> > CrossingEdgeRes;
+    std::map<int,IntersectionResult<double> > ReverseCrossingEdgeRes;
 
     DistPhysBAMIntersector &distIntersector;
     Vec<int> status; //<! Whether a node is inside the fluid domain or not
     Vec<double> &phi; //<! Pseudo phi value
-    Vec<Vec3D> locNorm;
     EdgeSet &edges;
-    LIST_ARRAY<PAIR<VECTOR<int,2>,IntersectionResult<double> > > edgeRes;
-    int nIntersect;
-    void updatePhi(int p, int q, IntersectionResult<double> &res, SVec<double, 3> &X, Vec<double> &phi,
-                   SVec<double, 3> &normApprox, Vec<double> &weightSum);
+    int nFirstLayer;
+
+//    void updatePhi(int p, int q, IntersectionResult<double> &res, SVec<double, 3> &X, Vec<double> &phi,
+//                   SVec<double, 3> &normApprox, Vec<double> &weightSum);
 
     /** compute projection of any point on the plane of a triangle.
      * send back the signed distance and barycentric coordinates of the projection point. */
@@ -108,19 +110,26 @@ class PhysBAMIntersector : public LevelSetStructure {
     /** Function to compute a signed distance and normal estimates for nodes that are next to the structure
      *
      * results are for the subdomain only */
-    void computeLocalPseudoPhi(SVec<double,3> &X, SVec<double,3> &n, Vec<double> &lWeight);
+    void computeFirstLayerNodeStatus(Vec<int> tId, Vec<double> dist);
+    /** compute the status (inside / outside) of nodes close to interface. */
+    void fixUntouchedSubDomain(SVec<double,3>&X);
+    /** if a subdomain has no intersection, find if it's (entirely) inside or outside. */
+    void finishNodeStatus(SubDomain& sub, SVec<double,3>&X);
+    /** compute the status (inside / outside) for all the nodes. */
+    void findIntersections(SVec<double,3>&X);
+    /** find intersections for each edge that has nodes with different statuses */
+
+//    void computeLocalPseudoPhi(SVec<double,3> &X, SVec<double,3> &n, Vec<double> &lWeight);
     /** complete the computation of the signed distance after communication with neighboring subdomains
      * has been done
      *
      * status is created and the locNorm contains our estimate of the normal for nodes near the structure*/
-    void finishPseudoPhi(SubDomain &sub, SVec<double,3> &X, SVec<double,3> &n, Vec<double> &lWeight);
+//    void finishPseudoPhi(SubDomain &sub, SVec<double,3> &X, SVec<double,3> &n, Vec<double> &lWeight);
 
     LevelSetResult
     getLevelSetDataAtEdgeCenter(double t, int ni, int nj);
     bool isActive(double t, int n);
     bool edgeIntersectsStructure(double t, int ni, int nj) const;
-
-
 
 };
 
