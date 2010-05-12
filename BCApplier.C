0de785cd505131b1d8a8297b663446f585734b3d
Philip Avery <pavery@stanford.edu>
2010-05-12 13:03:31 -0700

    fixed bug in BCApplier::applyD

--- BCApplier.C
+++ BCApplier.C
@@ -53,7 +53,7 @@ BCApplier::applyD(DistSVec<double,dim> &X)
         for(int l=0; l<dim; l++)
           //PJSA FIX: this projector should be for the constraints from the fluid-structure interface
           // and not the sliding planes which are dealt with in applyP
-          if(subDofType[i][l]==BC_MATCHED) { nn++; x[i][l] = 0.0; } 
+          if(subDofType[i][l]==BC_MATCHED || subDofType[i][l]==BC_FIXED) { nn++; x[i][l] = 0.0; } 
           // if(subDofType[i][l]!=BC_FREE) { nn++; x[i][l] = 0.0; }
   
       //fprintf(stderr, "sub %d zeroed %d of %d dofs\n", subD[iSub]->getGlobSubNum(), nn, X.subSize(iSub)*dim);
