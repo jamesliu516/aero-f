88aaf94a3cc8f106cdf87182cdf90170f8a7ce55
Adam Larat <adam.larat@stanford.edu>
2011-01-20 16:21:55 -0800

    imported patch leakSAExplicit

--- DistBcData.h
+++ DistBcData.h
@@ -91,7 +91,7 @@ protected:
 public:
 
   DistBcData(IoData &, VarFcn *, Domain *);
-  ~DistBcData();
+  virtual ~DistBcData();
 
   BcData<dim> &operator() (int i) const { return *subBcData[i]; }
 
