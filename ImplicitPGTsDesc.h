848dd7c569358e40357ddc2ec678508bc2bf795a
David Amsallem <amsallem@stanford.edu>
2011-01-26 17:37:10 -0800

    Added Petrov Galerkin by LS

--- ImplicitPGTsDesc.h
+++ ImplicitPGTsDesc.h
@@ -2,7 +2,8 @@
 #define _IMPLICIT_PG_TS_DESC_H_
 
 #include <ImplicitRomTsDesc.h>
-
+#include <ParallelRom.h>
+#include <VectorSet.h>
 //------------------------------------------------------------------------------
 
 template<int dim>
@@ -10,7 +11,8 @@ class ImplicitPGTsDesc : public ImplicitRomTsDesc<dim> {
 
 protected:
 
-	Vec<double> rhs;
+  ParallelRom<dim> parallelRom;
+  Vec<double> rhs;
   Vec<double> From;
   void saveAJsol(const int);	// only implemented for PG
 	void solveNewtonSystem(const int &it, double &res, bool &breakloop);
