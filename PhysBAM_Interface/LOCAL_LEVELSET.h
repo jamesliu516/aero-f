88e8ff325c888e396273986289f4bc20d03327e5
kevinwgy <kevinwgy@su-ahpcrc.stanford.edu>
2011-02-09 12:31:15 -0800

    added missing file

---
+++ PhysBAM_Interface/LOCAL_LEVELSET.h
@@ -0,0 +1,13 @@
+#ifndef __LOCAL_LEVELSET__
+#define __LOCAL_LEVELSET__
+#include<stdio.h>
+
+class LocalLevelSet {
+public:
+  virtual double getPhi(int trId, double xi1, double xi2, bool* hasCracked=0) {
+    fprintf(stderr,"ERROR: function getPhi is not implemented in LocalLevelSet!\n");
+    return 0; 
+  }
+}; 
+
+#endif
