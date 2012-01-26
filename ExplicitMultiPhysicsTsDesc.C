93e0163a28aab4025564f697a6e7e5bf51df48dc
Kevin Wang <kevin.wgy@gmail.com>
2012-01-26 12:16:20 -0800

    Minor change in the FS Riemann solver for Tait-Structure

--- ExplicitMultiPhysicsTsDesc.C
+++ ExplicitMultiPhysicsTsDesc.C
@@ -86,7 +86,8 @@ void ExplicitMultiPhysicsTsDesc<dim,dimLS>::solveNLSystemTwoBlocks(DistSVec<doub
   updatePhaseChangeFF(U);
   // check the consistency of Phi and FluidId. Can be removed for better efficiency! 
   this->LS->conservativeToPrimitive(this->Phi, this->PhiV, U);
-  this->domain->debugMultiPhysics(*(this->distLSS), this->PhiV, *(this->fluidSelector.fluidId), U);
+  if(this->withCracking && this->withMixedLS)
+    this->domain->debugMultiPhysics(*(this->distLSS), this->PhiV, *(this->fluidSelector.fluidId), U);
 }
 
 //------------------------------------------------------------------------------
