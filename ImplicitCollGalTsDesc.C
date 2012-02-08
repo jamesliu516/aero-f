df00526e6f3d0136afcf2b8c3ac75e46a107f449
Jon Tomas Gretarsson <jontg@cs.stanford.edu>
2012-02-07 18:25:36 -0800

    Updated MultiLevel Extrapolation to work with the MultiPhysics code. Also removed some extranious left-over fprintfs.

---
+++ ImplicitCollGalTsDesc.C
@@ -0,0 +1,46 @@
+template<int dim>
+ImplicitCollGalTsDesc<dim>::ImplicitCollGalTsDesc(IoData &ioData, GeoSource &geoSource, Domain *dom) :
+  ImplicitCollLSTsDesc<dim>(ioData, geoSource, dom)
+{
+	podRestrict.reset(new VecSet<DistSVec<double, dim> >(this->nPod, this->getRestrictedDistInfo()));
+	for (int iPod = 0; iPod < this->nPod; iPod++) { 
+		this->restrictMapping()->restriction(this->pod[iPod], (*podRestrict)[iPod]);
+		//(*AJRestrict)[iPod] = this->AJ[iPod];
+	}
+}
+
+template<int dim>
+void ImplicitCollGalTsDesc<dim>::solveNewtonSystem(const int &it, double &res, bool &breakloop)  {
+
+		projectVector(*podRestrict, *(this->ResRestrict), this->From);	// different from PG
+		this->rhs = -1.0 * this->From;
+
+    res = this->rhs*this->rhs;
+
+    if (res < 0.0){
+      fprintf(stderr, "*** negative residual: %e\n", res);
+      exit(1);
+    }
+    res = sqrt(res);
+
+    if (it == 0) {
+      this->target = this->epsNewton*res;
+      this->res0 = res;
+    }
+
+    if (res == 0.0 || res <= this->target) {
+			breakloop = true;
+			return;	// do not solve the system
+		}
+
+		// form reduced Jacobian
+
+		transMatMatProd(*podRestrict,*(this->AJRestrict),this->jactmp);
+		for (int iRow = 0; iRow < this->nPod; ++iRow) {
+			for (int iCol = 0; iCol < this->nPod; ++iCol) {	// different from PG
+				this->jac[iRow][iCol] = this->jactmp[iRow + iCol * this->pod.numVectors()];
+			}
+		} 
+
+    solveLinearSystem(it, this->rhs, this->dUrom);
+}
---
+++ IoData.C
@@ -0,0 +1,55 @@
+#include <IoData.h>
+
+#include <parser/Assigner.h>
+
+#include<limits>
+
+//------------------------------------------------------------------------------
+
+template<class GenericKrylov>
+NewtonData<GenericKrylov>::NewtonData()
+{
+
+  failsafe = NO;
+  maxIts = 1;
+  eps = 1.e-2;
+	JacSkip = 1;
+  epsAbsRes = std::numeric_limits<double>::epsilon();
+  epsAbsInc = std::numeric_limits<double>::epsilon();
+}
+
+//------------------------------------------------------------------------------
+
+template<class GenericKrylov>
+void NewtonData<GenericKrylov>::setup(const char *name, ClassAssigner *father)
+{
+
+  ClassAssigner *ca = new ClassAssigner(name, 6, father);
+
+  new ClassToken<NewtonData>
+    (ca, "FailSafe", this, reinterpret_cast<int NewtonData::*>(&NewtonData::failsafe), 3,
+     "Off", 0, "On", 1, "AlwaysOn", 2);
+  new ClassInt<NewtonData>(ca, "MaxIts", this, &NewtonData::maxIts);
+  new ClassDouble<NewtonData>(ca, "Eps", this, &NewtonData::eps);
+	new ClassInt<NewtonData>(ca, "JacobianFrequency", this, &NewtonData::JacSkip);
+  //new ClassDouble<NewtonData>(ca, "EpsRelRes", this, &NewtonData::eps);
+  new ClassDouble<NewtonData>(ca, "EpsAbsRes", this, &NewtonData::epsAbsRes);
+  new ClassDouble<NewtonData>(ca, "EpsAbsInc", this, &NewtonData::epsAbsInc);
+
+  ksp.setup("LinearSolver", ca);
+
+}
+
+
+//------------------------------------------------------------------------------
+
+template<class DataType>
+void ObjectMap<DataType>::setup(const char *name, ClassAssigner *p)  {
+
+  SysMapObj<DataType> *smo = new SysMapObj<DataType>(name, &dataMap);
+
+  if (p) p->addSmb(name, smo);
+  else addSysSymbol(name, smo);
+
+}
+
