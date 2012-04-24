ca03685f78e98f2991886814a72eae5155a2e6a8
Jon Tomas Gretarsson <jontg@cs.stanford.edu>
2012-04-24 13:19:07 -0700

    BUG FIX: getPolygons(...) was the old, pre-09/2011 implementation >.< OPTIMIZATION: for swept node classification, use int instead of double for init and next_init. Removing old debug messages, and removing default arguments that aren't used.

--- PolygonReconstructionData.h
+++ PolygonReconstructionData.h
@@ -11,51 +11,67 @@ struct Vec3D;
 
 struct PolygonReconstructionData { //for force computation under the embedded framework
     PolygonReconstructionData() : numberOfEdges(0) {}
-    int numberOfEdges;
-    int edgeWithVertex[4][2];
+    int numberOfEdges,nodeToLookFrom;
     int edge[4];
+    int edgeWithVertex[4][2];
 
-    void AssignSingleEdge(int n1, int n2,int l1){ //for PhysBAM only
-        numberOfEdges=1;
-        edge[0]=l1;
-        edgeWithVertex[0][0]=n1; edgeWithVertex[0][1]=n2;
-    }
-
-    void AssignTwoEdges(int n1, int n2, int n3, int l1, int l2){ //for PhysBAM only
-        numberOfEdges=2;
-        edge[0]=l1; edge[1]=l2;
+    void AssignTriangleSingle(const int n1, const int n2, const int n3, const int n4, const int l1, const int l2, const int l3){
+        numberOfEdges=3; nodeToLookFrom=n1;
+        edge[0]=l1; edge[1]=l2; edge[2]=l3;
         edgeWithVertex[0][0]=n1; edgeWithVertex[0][1]=n2;
         edgeWithVertex[1][0]=n1; edgeWithVertex[1][1]=n3;
+        edgeWithVertex[2][0]=n1; edgeWithVertex[2][1]=n4;
     }
 
-    void AssignTriangle(int n1, int n2, int n3, int n4, int l1, int l2, int l3, bool owned_by_single_vertex=true){
-        numberOfEdges=3;
+    void AssignTriangleMulti(const int n1, const int n2, const int n3, const int n4, const int l1, const int l2, const int l3){
+        numberOfEdges=3; nodeToLookFrom=n2;
         edge[0]=l1; edge[1]=l2; edge[2]=l3;
-        if(owned_by_single_vertex){
-            edgeWithVertex[0][0]=n1; edgeWithVertex[0][1]=n2;
-            edgeWithVertex[1][0]=n1; edgeWithVertex[1][1]=n3;
-            edgeWithVertex[2][0]=n1; edgeWithVertex[2][1]=n4;}
-        else{
-            edgeWithVertex[0][0]=n2; edgeWithVertex[0][1]=n1;
-            edgeWithVertex[1][0]=n3; edgeWithVertex[1][1]=n1;
-            edgeWithVertex[2][0]=n4; edgeWithVertex[2][1]=n1;}
+        edgeWithVertex[0][0]=n2; edgeWithVertex[0][1]=n1;
+        edgeWithVertex[1][0]=n3; edgeWithVertex[1][1]=n1;
+        edgeWithVertex[2][0]=n4; edgeWithVertex[2][1]=n1;
     }
 
-    void AssignQuadTriangle(int n1, int n2, int n3, int n4, int l1, int l2, int l3){ //for PhysBAM only
-        numberOfEdges=3;
+    void AssignQuadrilateral(const int n1, const int n2, const int n3, const int n4, const int l1, const int l2, const int l3, const int l4){
+        numberOfEdges=4; nodeToLookFrom=n1;
+        edge[0]=l1; edge[1]=l2; edge[2]=l3; edge[3]=l4;
+        edgeWithVertex[0][0]=n1; edgeWithVertex[0][1]=n3;
+        edgeWithVertex[1][0]=n1; edgeWithVertex[1][1]=n4;
+        edgeWithVertex[2][0]=n2; edgeWithVertex[2][1]=n4;
+        edgeWithVertex[3][0]=n2; edgeWithVertex[3][1]=n3;
+    }
+
+    void AssignQuadTriangle(const int n1, const int n2, const int n3, const int n4, const int l1, const int l2, const int l3){ //for PhysBAM only
+        numberOfEdges=3; nodeToLookFrom=n1;
         edge[0]=l1; edge[1]=l2; edge[2]=l3;
         edgeWithVertex[0][0]=n1; edgeWithVertex[0][1]=n3;
         edgeWithVertex[1][0]=n1; edgeWithVertex[1][1]=n4;
         edgeWithVertex[2][0]=n2; edgeWithVertex[2][1]=n3;
     }
 
-    void AssignQuadrilateral(int n1, int n2, int n3, int n4, int l1, int l2, int l3, int l4){
-        numberOfEdges=4;
-        edge[0]=l1; edge[1]=l2; edge[2]=l3; edge[3]=l4;
-        edgeWithVertex[0][0]=n1; edgeWithVertex[0][1]=n3;
-        edgeWithVertex[1][0]=n1; edgeWithVertex[1][1]=n4;
-        edgeWithVertex[2][0]=n2; edgeWithVertex[2][1]=n4;
-        edgeWithVertex[3][0]=n2; edgeWithVertex[3][1]=n3;
+    void AssignConnectingQuadTriangle(const int n1, const int n2, const int n3, const int n4, const int l1, const int l2){ //for PhysBAM only
+        numberOfEdges=3; nodeToLookFrom=n3;
+        edge[0]=l1; edge[1]=l1; edge[2]=l2;
+        edgeWithVertex[0][0]=n1; edgeWithVertex[0][1]=n2;
+        edgeWithVertex[1][0]=n2; edgeWithVertex[1][1]=n1;
+        edgeWithVertex[2][0]=n3; edgeWithVertex[2][1]=n4;
+    }
+
+    void AssignTwoEdges(const int n1, const int n2, const int n3, const int n4, const int l1, const int l2){ //for PhysBAM only
+        numberOfEdges=4; nodeToLookFrom=n4;
+        edge[0]=l1; edge[1]=l2; edge[2]=l2; edge[3]=l1;
+        edgeWithVertex[0][0]=n1; edgeWithVertex[0][1]=n2;
+        edgeWithVertex[1][0]=n1; edgeWithVertex[1][1]=n3;
+        edgeWithVertex[2][0]=n3; edgeWithVertex[2][1]=n1;
+        edgeWithVertex[3][0]=n2; edgeWithVertex[3][1]=n1;
+    }
+
+    void AssignTwoEdgesPartTwo(const int n1, const int n2, const int n3, const int n4, const int l1, const int l2, const int l3){ //for PhysBAM only
+        numberOfEdges=4; nodeToLookFrom=n4;
+        edge[0]=l1; edge[1]=l3; edge[2]=l3; edge[3]=l2;
+        edgeWithVertex[0][0]=n2; edgeWithVertex[0][1]=n1;
+        edgeWithVertex[1][0]=n2; edgeWithVertex[1][1]=n3;
+        edgeWithVertex[2][0]=n3; edgeWithVertex[2][1]=n2;
+        edgeWithVertex[3][0]=n3; edgeWithVertex[3][1]=n1;
     }
 };
 
