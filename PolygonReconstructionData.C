2534967a0ee8bb4da2af4b5950cb48680f2e14a2
Kevin Wang <kevin.wgy@gmail.com>
2012-04-25 10:55:52 -0700

    removed unnecessary screen output

---
+++ PolygonReconstructionData.C
@@ -0,0 +1,123 @@
+#include <PolygonReconstructionData.h>
+
+#include <Elem.h>
+#include <LevelSet/LevelSetStructure.h>
+#include <Vector3D.h>
+
+#include <cstdlib>
+#include <cstdio>
+//------------------------------------------------------------------------------
+
+int getPolygons(Elem &elem, LevelSetStructure &LSS, PolygonReconstructionData* polygons)
+{
+    int numberOfPolygons=0;
+    int oppositeNodes[4][3] = {{1,2,3},{0,2,3},{0,1,3},{0,1,2}};
+    int T[4]; for (int i=0; i<4; ++i) T[i] = elem[i]; //nodes in a tet.
+    bool isBlocked[4][4], exit_early(true);
+    int edge[4][4];
+    for(int i=0; i<6; ++i) {
+        int l = elem.edgeNum(i);
+        int ni=elem.edgeEnd(i,0), nj=elem.edgeEnd(i,1);
+        isBlocked[ni][nj] = isBlocked[nj][ni] = LSS.edgeIntersectsStructure(0, l);
+        edge[ni][nj] = edge[nj][ni] = l;
+        if(isBlocked[ni][nj]) exit_early=false;}
+
+    if(exit_early) return 0; // No intersections detected; don't bother finding a reconstructed surface that does not exist.
+
+    bool finished[4]; for(int i=0; i<4; ++i) finished[i]=false;
+
+    // First, identify any of the simple cases
+    for(int current_node=0; current_node<4; ++current_node) {
+        int n0=oppositeNodes[current_node][0], n1=oppositeNodes[current_node][1], n2=oppositeNodes[current_node][2];
+        if(isBlocked[current_node][n0] && isBlocked[current_node][n1] && isBlocked[current_node][n2]) {
+            finished[current_node]=true;
+            polygons[numberOfPolygons++].AssignTriangleSingle(T[current_node], T[n0], T[n1], T[n2],
+                                                              edge[current_node][n0], edge[current_node][n1], edge[current_node][n2]);
+            if(!isBlocked[n0][n1] && !isBlocked[n0][n2] && !isBlocked[n1][n2]) { // REALLY simple case...
+                finished[n0] = finished[n1] = finished[n2] = true;
+                polygons[numberOfPolygons++].AssignTriangleMulti(T[current_node], T[n0], T[n1], T[n2],
+                                                                 edge[n0][current_node], edge[n1][current_node], edge[n2][current_node]);}}}
+
+    bool has_encountered_quad_triangle=false;
+    for(int current_node=0; current_node<4; ++current_node) {
+        if(finished[current_node]) continue;
+        for(int n=0; n<3; ++n) {
+            int neighbor_node=oppositeNodes[current_node][n], e0=oppositeNodes[current_node][(n+1)%3], e1=oppositeNodes[current_node][(n+2)%3];
+            if(!isBlocked[current_node][neighbor_node] && isBlocked[current_node][e0] && isBlocked[current_node][e1]) {
+                finished[current_node] = true;
+                if(isBlocked[neighbor_node][e0] && isBlocked[neighbor_node][e1]) {
+                    finished[neighbor_node] = true;
+                    polygons[numberOfPolygons++].AssignQuadrilateral(T[current_node], T[neighbor_node], T[e0], T[e1],
+                                                                     edge[current_node][e0], edge[current_node][e1],
+                                                                     edge[neighbor_node][e1], edge[neighbor_node][e0]);
+                    break;}
+                else if(isBlocked[neighbor_node][e0]) {
+                    polygons[numberOfPolygons++].AssignQuadTriangle(T[current_node], T[neighbor_node], T[e0], T[e1],
+                                                                    edge[current_node][e0], edge[current_node][e1], edge[neighbor_node][e0]);
+                    if(!isBlocked[e0][e1] || !has_encountered_quad_triangle) {
+                        polygons[numberOfPolygons++].AssignConnectingQuadTriangle(T[current_node], T[e1], T[neighbor_node], T[e0],
+                                                                                  edge[current_node][e1], edge[neighbor_node][e0]);}
+                    has_encountered_quad_triangle=true; break;}
+                else if(isBlocked[neighbor_node][e1]) {
+                    polygons[numberOfPolygons++].AssignQuadTriangle(T[current_node], T[neighbor_node], T[e1], T[e0],
+                                                                    edge[current_node][e1], edge[current_node][e0], edge[neighbor_node][e1]);
+                    if(!isBlocked[e0][e1] || !has_encountered_quad_triangle) {
+                        polygons[numberOfPolygons++].AssignConnectingQuadTriangle(T[current_node], T[e0], T[neighbor_node], T[e1],
+                                                                                  edge[current_node][e0], edge[neighbor_node][e1]);}
+                    has_encountered_quad_triangle=true; break;}
+                else{
+                    finished[neighbor_node] = finished[e0] = finished[e1] = true;
+                    polygons[numberOfPolygons++].AssignTwoEdges(T[current_node], T[e1], T[e0], T[neighbor_node],
+                                                                edge[current_node][e1], edge[current_node][e0]);
+                    if(isBlocked[e0][e1])
+                        polygons[numberOfPolygons++].AssignTwoEdgesPartTwo(T[current_node], T[e1], T[e0], T[neighbor_node],
+                                                                           edge[current_node][e1], edge[current_node][e0], edge[e0][e1]);
+                    break;}}}}
+
+    assert(numberOfPolygons);
+    return numberOfPolygons;
+}
+
+//--------------------------------------------------------------------------
+
+void getPolygonNormal(SVec<double,3>& X, Vec3D &normal, LevelSetStructure &LSS, PolygonReconstructionData &polygon)
+{
+  int nEdges = polygon.numberOfEdges;
+  LevelSetResult lsRes[nEdges];
+  Vec3D Xinter[nEdges];
+  Vec3D start_vertex;for(int m=0;m<3;++m) start_vertex[m]=X[polygon.nodeToLookFrom][m];
+  for(int m=0; m<nEdges; m++) {
+    lsRes[m] = LSS.getLevelSetDataAtEdgeCenter(0, polygon.edge[m], polygon.edgeWithVertex[m][0]<polygon.edgeWithVertex[m][1]);
+    double alpha = lsRes[m].alpha;
+    if (alpha<0) {fprintf(stderr,"Unable to get intersection results at edge center! Abort...\n"); exit(-1);}
+    Xinter[m][0] = alpha*X[polygon.edgeWithVertex[m][0]][0] + (1.0-alpha)*X[polygon.edgeWithVertex[m][1]][0];
+    Xinter[m][1] = alpha*X[polygon.edgeWithVertex[m][0]][1] + (1.0-alpha)*X[polygon.edgeWithVertex[m][1]][1];
+    Xinter[m][2] = alpha*X[polygon.edgeWithVertex[m][0]][2] + (1.0-alpha)*X[polygon.edgeWithVertex[m][1]][2];
+  }
+  switch(nEdges) {
+    case 3: // got a triangle.
+      normal = (Xinter[1]-Xinter[0])^(Xinter[2]-Xinter[0]);
+      if (normal.norm() != 0.0) {normal = 1.0/normal.norm()*normal;}
+      // Then we check the orientation of the normal.
+      for (int i=0; i<3; i++) {start_vertex[i] = X[polygon.edgeWithVertex[0][0]][i];}
+      if (LSS.isActive(0, polygon.edgeWithVertex[0][0])) {
+        if (normal*(start_vertex-Xinter[0]) < 0) {normal = -1.0*normal;}
+      } else if (normal*(start_vertex-Xinter[0]) > 0) {normal = -1.0*normal;}
+      break;
+    case 4: // got a quadrangle... We cut it into two triangles and we average the two resulting normals. 
+      Vec3D normal1, normal2;
+      normal1 = (Xinter[1]-Xinter[0])^(Xinter[3]-Xinter[0]);
+      if (normal1.norm() != 0.0) {normal1 = 1.0/normal1.norm()*normal1;}
+      normal2 = (Xinter[2]-Xinter[1])^(Xinter[3]-Xinter[1]);
+      if (normal2.norm() != 0.0) {normal2 = 1.0/normal2.norm()*normal2;}
+      normal = 0.5*(normal1+normal2);
+      if (normal.norm() != 0.0) {normal = 1.0/normal.norm()*normal;}
+      // Then we check the orientation of the normal.
+      for (int i=0; i<3; i++) {start_vertex[i] = X[polygon.edgeWithVertex[0][0]][i];}
+      if (LSS.isActive(0, polygon.edgeWithVertex[0][0])) {
+        if (normal*(start_vertex-Xinter[0]) < 0) {normal = -1.0*normal;}
+      } else if (normal*(start_vertex-Xinter[0]) > 0) {normal = -1.0*normal;}
+      break;
+  }
+}
+
