740ba8f9e45944f12968d519abec1c6836da969d
Kevin Wang <kevin.wgy@gmail.com>
2012-06-12 15:30:41 -0700

    Added Jon's code for computing wall-distance for embedded.

---
+++ DistTimeState.h
@@ -0,0 +1,209 @@
+#ifndef _DIST_TIME_STATE_H_
+#define _DIST_TIME_STATE_H_
+
+#include <TimeData.h>
+#include <DistVector.h>
+#include <DistMacroCell.h>
+#include <LowMachPrec.h>
+#include <LevelSet/LevelSetStructure.h>
+
+struct FluidModelData;
+struct InitialConditions;
+
+class VarFcn;
+class Domain;
+class DistGeoState;
+class FemEquationTerm;
+class DistLevelSetStructure;
+
+template<int dim> class TimeState;
+template<class Scalar, int dim> class DistMat;
+template<int dim> class SpaceOperator;
+template<int dim> class DistExactRiemannSolver;
+
+//------------------------------------------------------------------------------
+
+template<int dim>
+class DistTimeState {
+
+private:
+
+  VarFcn *varFcn;
+  FemEquationTerm *fet;
+  DistSVec<double,dim> *V;
+  DistSVec<double,dim> *Vn;
+  DistSVec<double,dim> *VnBar;
+  DistSVec<double,dim> *QBar;
+
+  bool locAlloc;
+  int numLocSub;
+
+  double gam;
+  double pstiff;
+
+  TimeLowMachPrec    tprec;
+  SpatialLowMachPrec sprec; //only for computation of irey
+  /*bool prec;
+  double beta;
+  double mach;
+  double cmach;
+  double k1;
+  double betav;
+*/
+  double viscousCst;
+
+  TimeData *data;
+
+  DistVec<double> *dt;			//actual   time stepping
+  DistVec<double> *idti;		//inverse inviscid time stepping
+  DistVec<double> *idtv;		//inverse viscous  time stepping
+  DistVec<double> *irey;
+  DistSVec<double,dim> *Un;
+  DistSVec<double,dim> *Unm1;
+  DistSVec<double,dim> *Unm2;
+  DistSVec<double,dim> *UnBar;
+  DistSVec<double,dim> *Unm1Bar;
+  DistSVec<double,dim> *Unm2Bar;
+  DistSVec<double,dim> *Rn;
+  double errorEstiNorm;                 //norm of estimated error
+  double dtMin;
+
+  Domain *domain;
+
+  TimeState<dim> **subTimeState;
+
+// Included (MB)
+  DistVec<double> *dIrey;
+  DistVec<double> *dIdti;
+  DistVec<double> *dIdtv;
+
+  bool isGFMPAR;
+  int fvmers_3pbdf ;
+
+private:
+  void computeInitialState(InitialConditions &ic, FluidModelData &fm, double UU[dim]);
+public:
+
+  DistTimeState(IoData &, SpaceOperator<dim> *, VarFcn *, Domain *, DistSVec<double,dim> * = 0);
+  DistTimeState(const DistTimeState<dim> &, bool, IoData &);
+  ~DistTimeState();
+
+  TimeState<dim> &operator() (int i) const { return *subTimeState[i]; }
+
+  void setResidual(DistSVec<double,dim> *rn) { if (Rn != 0) delete Rn; Rn = rn; }
+
+  void setGlobalTimeStep (double t) { *dt = t; }
+
+  void setup(const char *name, DistSVec<double,3> &X, DistSVec<double,dim> &Ufar,
+             DistSVec<double,dim> &U, IoData &iod, DistVec<int> *fluidId = 0); 
+  void setupUVolumesInitialConditions(IoData &iod);
+  void setupUOneDimensionalSolution(IoData &iod, DistSVec<double,3> &X);
+  void setupUMultiFluidInitialConditions(IoData &iod, DistSVec<double,3> &X);
+  void setupUFluidIdInitialConditions(IoData &iod, DistVec<int> &fluidId);
+  void update(DistSVec<double,dim> &,bool increasingPressure = false);
+  void update(DistSVec<double,dim> &Q,  DistSVec<double,dim> &Qtilde,DistVec<int> &fluidId, DistVec<int> *fluidIdnm1, 
+              DistExactRiemannSolver<dim> *riemann,class DistLevelSetStructure* = 0, bool increasingPressure = false);
+
+  void writeToDisk(char *);
+
+  double computeTimeStep(double, double*, int*, DistGeoState &, 
+			 DistSVec<double,3> &, DistVec<double> &, DistSVec<double,dim> &);
+  double computeTimeStep(double, double*, int*, DistGeoState &,
+                         DistVec<double> &, DistSVec<double,dim> &, DistVec<int> &, DistVec<double>* = NULL);
+
+  //computes time step size when time step failed
+  double computeTimeStepFailSafe(double* , int*);
+
+  //computes time step with error estimation
+  //see: "Adaptive Time Stepping for Non-Linear Hyperbolic Problems"
+  double computeTimeStep(int, double* , int*);
+
+  //calculates the norm of the error estimator
+  void calculateErrorEstiNorm(DistSVec<double,dim> &, DistSVec<double,dim> &);
+
+  //minimum time step size is set for error estimation
+  void setDtMin(double dt){dtMin = dt;}
+
+  void computeCoefficients(double);
+
+  void add_dAW_dt(int, DistGeoState &, DistVec<double> &, 
+		  DistSVec<double,dim> &, DistSVec<double,dim> &, DistLevelSetStructure *distLSS=0);
+  void add_dAW_dtRestrict(int, DistGeoState &, DistVec<double> &, 
+			  DistSVec<double,dim> &, DistSVec<double,dim> &, const std::vector<std::vector<int> > &);
+  template<int dimLS>
+  void add_dAW_dtLS(int, DistGeoState &, DistVec<double> &, 
+			 DistSVec<double,dimLS> &, DistSVec<double,dimLS> &, DistSVec<double,dimLS> &, 
+			 DistSVec<double,dimLS> &, DistSVec<double,dimLS> &,bool requireSpecialBDF = false);
+
+  template<class Scalar, int neq>
+  void addToJacobian(DistVec<double> &, DistMat<Scalar,neq> &, DistSVec<double,dim> &);
+
+  template<class Scalar, int neq>
+  void addToJacobianNoPrec(DistVec<double> &, DistMat<Scalar,neq> &, DistSVec<double,dim> &);
+
+  template<class Scalar, int neq>
+  void addToJacobianLS(DistVec<double> &, DistMat<Scalar,neq> &, DistSVec<double,dim> &,bool);
+
+  template<class Scalar, int neq>
+  void addToJacobianGasPrec(DistVec<double> &, DistMat<Scalar,neq> &, DistSVec<double,dim> &);
+
+  template<class Scalar, int neq>
+  void addToJacobianLiquidPrec(DistVec<double> &, DistMat<Scalar,neq> &, DistSVec<double,dim> &);
+
+  template<class Scalar, int neq>
+  void addToH1(DistVec<double> &, DistMat<Scalar,neq> &);
+
+  template<class Scalar, int neq>
+  void addToH1(DistVec<double> &, DistMat<Scalar,neq> &, Scalar);
+
+  template<class Scalar, int neq>
+  void addToH2(DistVec<double> &, DistSVec<double,dim> &, DistMat<Scalar,neq> &);
+
+  template<class Scalar, int neq>
+  void addToH2(DistVec<double> &, DistSVec<double,dim> &, DistMat<Scalar,neq> &, Scalar);
+
+  template<class Scalar, int neq>
+  void addToH2(DistVec<double> &, DistSVec<double,dim> &, DistMat<Scalar,neq> &, Scalar, double);
+  
+  template<class Scalar, int neq>
+  void addToH2Minus(DistVec<double> &, DistSVec<double,dim> &, DistMat<Scalar,neq> &);
+
+  void computeBar(bool, DistMacroCellSet *, DistGeoState &, int);
+                                                                                                                          
+  void get_dW_dt(bool, DistGeoState &, DistVec<double> &, DistSVec<double,dim> &,
+                 DistSVec<double,dim> &, DistSVec<double,dim> &, DistSVec<double,dim> &,
+                 DistMacroCellSet *, DistSVec<double,1> **, int);
+
+  DistVec<double>* getInvReynolds(){ return irey; }
+                                                                                                                          
+  void multiplyByTimeStep(DistSVec<double,dim>&);
+  template<int dimLS>
+  void multiplyByTimeStep(DistSVec<double,dimLS>&);
+  void multiplyByPreconditioner(DistSVec<double,dim> &, DistSVec<double,dim>&);
+  void multiplyByPreconditionerPerfectGas(DistSVec<double,dim> &, DistSVec<double,dim>&);
+  void multiplyByPreconditionerLiquid(DistSVec<double,dim> &, DistSVec<double,dim>&);
+
+  TimeData &getData() { return *data; }
+  DistSVec<double,dim> &getUn() const { return *Un; }
+  DistSVec<double,dim> &getUnm1() const { return *Unm1; }
+
+  inline bool existsNm1() const { return data->exist_nm1; }
+  inline bool useNm1() const { return data->use_nm1; }
+
+  double getTime()  { return data->dt_n; }
+
+  void rstVar(IoData &);
+
+  DistVec<double>* getDerivativeOfInvReynolds(DistGeoState &, DistSVec<double,3> &, DistSVec<double,3> &, DistVec<double> &, DistVec<double> &, DistSVec<double,dim> &, DistSVec<double,dim> &, double);
+
+	int getOutputNewtonStep() const;
+
+};
+
+//------------------------------------------------------------------------------
+
+#ifdef TEMPLATE_FIX
+#include <DistTimeState.C>
+#endif
+
+#endif
---
+++ PostOperator.h
@@ -0,0 +1,202 @@
+#ifndef _POST_OPERATOR_H_
+#define _POST_OPERATOR_H_
+
+#include <PostFcn.h>
+#include <VectorSet.h>
+#include <GhostPoint.h>
+#include <map>
+#include <LevelSet/LevelSetStructure.h>
+
+using std::map;
+
+
+class IoData;
+class VarFcn;
+class SubDomain;
+class Domain;
+class DistGeoState;
+class Communicator;
+class SmagorinskyLESTerm;
+class WaleLESTerm;
+class DynamicLESTerm;
+
+struct Vec3D;
+
+template<int dim> class DistBcData;
+template<class Scalar> class DistVec;
+template<class Scalar, int dim> class DistSVec;
+template<int dim> class DistVMSLESTerm;
+template<int dim> class DistDynamicLESTerm;
+template<int dim> class DistDynamicVMSTerm;
+template<int dim> class SpaceOperator;
+template<int dim> class DistTimeState;
+template<int dim> class DistExactRiemannSolver;
+
+
+template<int dim>
+class ForceGenerator {
+  public:
+    virtual void getForcesAndMoments(DistSVec<double,dim> &U, DistSVec<double,3> &X,
+                                           double F[3], double M[3]) = 0;
+};
+
+//------------------------------------------------------------------------------
+
+template<int dim>
+class PostOperator {
+
+  VarFcn *varFcn;
+  DistBcData<dim> *bcData;
+  DistGeoState *geoState;
+  DistSVec<double,dim> *V;
+
+  int numLocSub;
+  SubDomain **subDomain;
+  Domain *domain;
+  Communicator *com;
+  int numSurf;
+  int numSurfHF;
+  map<int,int> surfOutMap;
+  map<int,int> surfOutMapHF;
+  map<int,int> surfComputeMap; //AS far as I can figure out this map is never used
+  ForceGenerator<dim> *forceGen;
+  
+ // Coefficients to Compute nodal force transfer
+  double nodalForceWeights[2];
+
+// Included (MB)
+  DistSVec<double,dim> *dV;
+
+private:
+
+  double threshold;
+  double refLengthSq;
+  double pressInfty;
+  DistSVec<double,2>* tmp2;
+  SmagorinskyLESTerm *smag;
+  WaleLESTerm *wale;  
+  DistVMSLESTerm<dim> *vms;
+  DistDynamicLESTerm<dim> *dles;
+  DistDynamicVMSTerm<dim> *dvms;
+  SpaceOperator<dim> *spaceOp;
+  CommPattern<double>* vec2Pat;
+  PostFcn *postFcn;
+  DistVec<double> *mutOmu;
+  DistVec<double> *Cs;
+  DistVec<double> *CsDvms;
+  DistVec<double> *CsDles;
+
+public:
+
+  PostOperator(IoData &, VarFcn *, DistBcData<dim> *, DistGeoState *, 
+	       Domain *, DistSVec<double,dim> * = 0);
+  ~PostOperator();
+
+  void computeNodalForce(DistSVec<double,3> &, DistSVec<double,dim> &, 
+			 DistVec<double> &, DistSVec<double,3> &,
+			 DistVec<int> * = 0);
+
+  void computeNodalHeatPower(DistSVec<double,3> &, DistSVec<double,dim> &, 
+			     DistVec<double> &);
+  void computeNodalHeatFluxRelatedValues(DistSVec<double,3> &, DistSVec<double,dim> &,
+                                               DistVec<double> &, bool includeKappa);
+  void computeForceAndMoment(Vec3D &, DistSVec<double,3> &, DistSVec<double,dim> &,
+                             DistVec<int> *,
+			     Vec3D *, Vec3D *, Vec3D *, Vec3D *, int = 0, 
+                             VecSet< DistSVec<double,3> > *mX = 0, Vec<double> *genCF = 0);
+  void computeForceAndMoment(DistExactRiemannSolver<dim>&, 
+                             Vec3D &, DistSVec<double,3> &, DistSVec<double,dim> &,
+                             DistVec<int> *,
+                             Vec3D *, Vec3D *, Vec3D *, Vec3D *, int = 0,
+                             VecSet< DistSVec<double,3> > *mX = 0, Vec<double> *genCF = 0);
+
+  void computeHeatFluxes(DistSVec<double,3> &,
+                                          DistSVec<double,dim> &, double*);
+
+  double computeInterfaceWork(DistSVec<double,3>&, DistSVec<double,dim>&, DistVec<double>&);
+
+  void computeScalarQuantity(PostFcn::ScalarType, DistSVec<double,3> &,
+			     DistSVec<double,dim> &, DistVec<double> &, 
+                             DistVec<double> &, DistTimeState<dim> *);
+  template<int dimLS>
+  void computeScalarQuantity(PostFcn::ScalarType, DistSVec<double,3> &,
+                             DistSVec<double,dim> &, DistVec<double> &,
+                             DistVec<double> &, DistTimeState<dim> *,
+                             DistVec<int> &,DistSVec<double,dimLS>* = NULL);
+
+  template<int dimLS>
+  void computeScalarQuantity(PostFcn::ScalarType, DistSVec<double,3> &,
+			     DistSVec<double,dim> &, DistVec<double> &,
+			     DistTimeState<dim> *,
+			     DistVec<int> &,int* subId,int* locNodeId,
+			     int* last,int count, double* result,
+                             std::vector<Vec3D>& locations,
+			     DistSVec<double,dimLS>* = NULL,
+                             DistLevelSetStructure *distLSS = 0,
+                             DistVec<GhostPoint<dim>*> *ghostPoints = 0);
+
+   void computeCP(DistSVec<double,3>& X, DistSVec<double,dim>& U, Vec3D &cp);
+  //void computeScalarQuantity(PostFcn::ScalarType, DistSVec<double,3> &,
+  //                           DistSVec<double,dim> &, DistVec<double> &,
+  //                           DistSVec<double,1> &, DistVec<int> &);
+
+  void computeVectorQuantity(PostFcn::VectorType, DistSVec<double,3> &,
+			     DistSVec<double,dim> &, DistSVec<double,3> &);
+  void computeVectorQuantity(PostFcn::VectorType, DistSVec<double,3> &,
+                             DistSVec<double,dim> &, DistSVec<double,3> &, DistVec<int> &);
+
+  void computeVectorQuantity(PostFcn::VectorType, DistSVec<double,3> &,
+                             DistSVec<double,dim> &,
+			     int* subId,int* locNodeId,int* last,
+			     int count, double* results, 
+                             std::vector<Vec3D>& locations,
+                             DistVec<int> &, DistLevelSetStructure *distLSS = 0,
+                             DistVec<GhostPoint<dim>*> *ghostPoints = 0);
+
+  void computeForceDerivs(DistSVec<double,3> &, DistSVec<double,dim> &,
+                          DistSVec<double,dim> &,Vec<double> &,VecSet< DistSVec<double, 3> > &);
+
+  void computeForceCoefficients(Vec3D &, DistSVec<double,3> &, DistSVec<double,dim> &,
+                                Vec3D &, Vec3D &, Vec3D &, Vec3D &,  
+                                VecSet< DistSVec<double,3> > *mX = 0, DistVec<double> *genCF = 0);
+  int getNumSurf() { return numSurf; }
+  map<int, int> &getSurfMap() { return surfOutMap; }
+
+  int getNumSurfHF() { return numSurfHF; }
+  map<int, int> &getSurfMapHF() { return surfOutMapHF; }
+
+  PostFcn* getPostFcn() {return postFcn;}
+  
+
+// Included (MB)
+  void computeDerivativeOfScalarQuantity(PostFcn::ScalarDerivativeType, double [3], DistSVec<double,3> &, DistSVec<double,3> &, DistSVec<double,dim> &, DistSVec<double,dim> &, DistVec<double> &, DistTimeState<dim> *);
+
+  void computeDerivativeOfVectorQuantity(PostFcn::VectorDerivativeType, DistSVec<double,3> &, DistSVec<double,3> &, DistSVec<double,dim> &, DistSVec<double,dim> &, DistSVec<double,3> &);
+
+  void computeDerivativeOfForceAndMoment(Vec3D &, DistSVec<double,3> &, DistSVec<double,3> &,
+                                                                           DistSVec<double,dim> &, DistSVec<double,dim> &, double [3],
+                                                                           Vec3D *, Vec3D *, Vec3D *, Vec3D *, int = 0);
+
+  void computeDerivativeOfNodalForce(DistSVec<double,3> &, DistSVec<double,3> &,
+                                                                DistSVec<double,dim> &, DistSVec<double,dim> &,
+                                                                DistVec<double> &, double [3], DistSVec<double,3> &);
+								
+  void rstVar(IoData &iod) {pressInfty = iod.aero.pressure;}								
+
+  void rstVarPostFcn(IoData &ioData) {postFcn->rstVar(ioData, com);}								
+
+  void computeDerivativeOfNodalHeatPower(DistSVec<double,3> &, DistSVec<double,3> &, DistSVec<double,dim> &, DistSVec<double,dim> &, double [3], DistVec<double> &);
+
+  void checkVec(DistSVec<double,3> &);
+
+  void setForceGenerator(ForceGenerator<dim> *fg) { forceGen = fg; }
+};
+
+//------------------------------------------------------------------------------
+
+#ifdef TEMPLATE_FIX
+#include <PostOperator.C>
+#endif
+
+#endif
+
