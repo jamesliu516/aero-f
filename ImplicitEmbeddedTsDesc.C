a1a854bb22afd0caa4778aa4e4deaedac0834bbc
Vinod Lakshminarayan <vinodkl@stanford.edu>
2013-04-04 13:27:26 -0700

    Implememted segregated solver for RANS equation in Embedded framework.

--- ImplicitEmbeddedTsDesc.C
+++ ImplicitEmbeddedTsDesc.C
@@ -34,21 +34,6 @@ ImplicitEmbeddedTsDesc(IoData &ioData, GeoSource &geoSource, Domain *dom):
   epsAbsResNewton = implicitData.newton.epsAbsRes;
   epsAbsIncNewton = implicitData.newton.epsAbsInc;
 
-  // MatVecProd, Prec and Krylov solver for Euler equations
-  if (implicitData.mvp == ImplicitData::FD)
-    mvp = new MatVecProdFD<dim,dim>(implicitData,this->timeState, this->geoState,
-                                      this->spaceOp,this->domain,ioData);
-  else if (implicitData.mvp == ImplicitData::H1)
-    mvp = new MatVecProdH1<dim,double,dim>(this->timeState, this->spaceOp, this->domain,ioData);
-  else{
-    this->com->fprintf(stdout, "*** Error: MatVecProdH2 is not available\n");
-    exit(1);
-  }
-  
-  pc = createPreconditioner<dim>(implicitData.newton.ksp.ns.pc, this->domain);
-  
-  ksp = createKrylovSolver(this->getVecInfo(), implicitData.newton.ksp.ns, mvp, pc, this->com);
-  
   //initialize emmh (EmbeddedMeshMotionHandler).
   if(this->dynNodalTransfer) 
     {
@@ -64,21 +49,6 @@ ImplicitEmbeddedTsDesc(IoData &ioData, GeoSource &geoSource, Domain *dom):
       this->emmh = 0;
     }
 
-  typename MatVecProd<dim,dim>::_fsi fsi = {
-
-    this->distLSS,
-    &this->nodeTag,
-    this->riemann,
-    this->linRecAtInterface,
-    this->viscSecOrder,
-    this->Nsbar,
-    &this->Wtemp,
-    this->riemannNormal,
-    this->ghostPoints,
-  };
-
-  mvp->AttachStructure(fsi);
-  
   this->existsWstarnm1 = false;
 
   if (ioData.problem.framework==ProblemData::EMBEDDEDALE && this->emmh) 
@@ -93,9 +63,6 @@ template<int dim>
 ImplicitEmbeddedTsDesc<dim>::~ImplicitEmbeddedTsDesc()
 {
   if (tag)   delete tag;
-  if (mvp)   delete mvp;
-  if (pc)    delete pc;
-  if (ksp)   delete ksp;
   if (ns)    delete ns;
 
 }
@@ -380,88 +347,3 @@ void ImplicitEmbeddedTsDesc<dim>::resetFixesTag()
 }
 
 //------------------------------------------------------------------------------
-template<int dim>
-void ImplicitEmbeddedTsDesc<dim>::computeJacobian(int it, DistSVec<double,dim> &Q,
-							DistSVec<double,dim> &F)
-{
-
-  MatVecProdH1<dim,double,dim> *mvph1 = dynamic_cast<MatVecProdH1<dim,double,dim> *>(mvp);
-  if (mvph1)  {
-    mvph1->clearGhost(); 
-  }
-
-  mvp->evaluate(it,*(this->X) ,*(this->A), Q, F);
-
-  mvph1 = dynamic_cast<MatVecProdH1<dim,double,dim> *>(mvp);
-//  if (mvph1 && this->ghostPoints) 
-//    this->domain->populateGhostJacobian(*this->ghostPoints,Q, this->varFcn, *this->distLSS, this->nodeTag,*mvph1);
-
-}
-//------------------------------------------------------------------------------
-template<int dim>
-void ImplicitEmbeddedTsDesc<dim>::setOperators(DistSVec<double,dim> &Q)
-{
-  
-  DistMat<double,dim> *_pc = dynamic_cast<DistMat<double,dim> *>(pc);
-  
-  if (_pc) {
-    
-    MatVecProdFD<dim,dim> *mvpfd = dynamic_cast<MatVecProdFD<dim,dim> *>(mvp);
-    MatVecProdH1<dim,double,dim> *mvph1 = dynamic_cast<MatVecProdH1<dim,double,dim> *>(mvp);
-    
-    if (mvpfd) {
-
-      this->spaceOp->computeJacobian(*this->X, *this->A, Q,this->distLSS, this->nodeTag, this->riemann,
-                                     this->riemannNormal, this->Nsbar,this->ghostPoints, *_pc,this->timeState);
-      this->timeState->addToJacobian(*this->A, *_pc, Q);
-      this->spaceOp->applyBCsToJacobian(Q, *_pc);
-    }
-    else if (mvph1) {
-      JacobiPrec<double,dim> *jac = dynamic_cast<JacobiPrec<double,dim> *>(pc);
-      IluPrec<double,dim> *ilu = dynamic_cast<IluPrec<double,dim> *>(pc);
-      
-      if (jac)
-	jac->getData(*mvph1);
-      else if (ilu)
-	ilu->getData(*mvph1);
-    }
-    
-  }
-  
-  double t0 = this->timer->getTime();
-  
-  pc->setup();
-  
-  double t = this->timer->addPrecSetupTime(t0);
-  
-  this->com->printf(6, "Fluid preconditioner computation: %f s\n", t);
-  
-}
-
-//------------------------------------------------------------------------------
-template<int dim>
-int ImplicitEmbeddedTsDesc<dim>::solveLinearSystem(int it, DistSVec<double,dim> &b,
-				                   DistSVec<double,dim> &dQ)
-{
- 
-  double t0 = this->timer->getTime();  
-
-  embeddeddQ = 0.0;
-  embeddedB.ghost() = 0.0;
-  embeddedB.real() = b;
-  
-  ksp->setup(it, this->maxItsNewton, embeddedB);
-  
-  int lits = ksp->solve(embeddedB, embeddeddQ);
-
-  if(this->data->checklinsolve && lits==ksp->maxits) this->data->badlinsolve=true;
- 
-  dQ = embeddeddQ.real();
-  embeddedU.ghost() += embeddeddQ.ghost();
-  
-  this->timer->addKspTime(t0);
-  
-  return lits;
-  
-}
-
