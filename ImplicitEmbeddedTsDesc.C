db33084d069d6e395424e9ef5096d6b41a77b096
Julien Cortial <jcortia@sandia.gov>
2013-04-26 09:28:08 -0700

    Fixed linking order
    If present, the Blacs libraries should be linked after the Scalapack one
    so that even a non-backtracking linker correctly satisfies dependencies.

---
+++ ImplicitEmbeddedTsDesc.C
@@ -0,0 +1,349 @@
+#include "IoData.h"
+#include "GeoSource.h"
+#include "Domain.h"
+#include "LevelSet.h"
+#include "DistTimeState.h"
+
+#include <MatVecProd.h>
+#include <KspSolver.h>
+#include <SpaceOperator.h>
+#include <NewtonSolver.h>
+
+
+#ifdef TYPE_PREC
+#define PrecScalar TYPE_PREC
+#else
+#define PrecScalar double
+#endif
+
+//------------------------------------------------------------------------------
+
+template<int dim>
+ImplicitEmbeddedTsDesc<dim>::
+ImplicitEmbeddedTsDesc(IoData &ioData, GeoSource &geoSource, Domain *dom):
+  EmbeddedTsDesc<dim>(ioData, geoSource, dom), embeddedU(dom->getNodeDistInfo()), embeddedB(dom->getNodeDistInfo()),embeddeddQ(dom->getNodeDistInfo())
+{
+  tag = 0;
+  ImplicitData &implicitData = ioData.ts.implicit;
+  
+  // NewtonSolver
+  ns = new NewtonSolver<ImplicitEmbeddedTsDesc<dim> >(this);
+  failSafeNewton = implicitData.newton.failsafe;
+  maxItsNewton = implicitData.newton.maxIts;
+  epsNewton = implicitData.newton.eps;
+  epsAbsResNewton = implicitData.newton.epsAbsRes;
+  epsAbsIncNewton = implicitData.newton.epsAbsInc;
+
+  //initialize emmh (EmbeddedMeshMotionHandler).
+  if(this->dynNodalTransfer) 
+    {
+      /*
+      MeshMotionHandler *_mmh = 0;
+      _mmh = new EmbeddedMeshMotionHandler(ioData, dom, this->dynNodalTransfer, this->distLSS);
+      this->emmh = _mmh;
+      */
+      this->emmh = new EmbeddedMeshMotionHandler(ioData, dom, this->dynNodalTransfer, this->distLSS);
+    } 
+  else
+    { 
+      this->emmh = 0;
+    }
+
+  this->existsWstarnm1 = false;
+
+  if (ioData.problem.framework==ProblemData::EMBEDDEDALE && this->emmh) 
+    this->mmh = this->createEmbeddedALEMeshMotionHandler(ioData, geoSource, this->distLSS);
+  else
+    this->mmh = 0;
+}
+
+//------------------------------------------------------------------------------
+
+template<int dim>
+ImplicitEmbeddedTsDesc<dim>::~ImplicitEmbeddedTsDesc()
+{
+  if (tag)   delete tag;
+  if (ns)    delete ns;
+
+}
+
+//------------------------------------------------------------------------------
+//  Internal routines to setup the class (called in constructor)
+//------------------------------------------------------------------------------
+
+template<int dim>
+template <int neq>
+KspPrec<neq> *ImplicitEmbeddedTsDesc<dim>::createPreconditioner(PcData &pcdata, Domain *dom)
+{
+  
+  KspPrec<neq> *_pc = 0;
+  
+  if (pcdata.type == PcData::IDENTITY)
+    _pc = new IdentityPrec<neq>();
+  else if (pcdata.type == PcData::JACOBI)
+    _pc = new JacobiPrec<double,neq>(DiagMat<double,neq>::DENSE, dom);
+  else if (pcdata.type == PcData::AS ||
+	   pcdata.type == PcData::RAS ||
+	   pcdata.type == PcData::ASH ||
+	   pcdata.type == PcData::AAS)
+    _pc = new IluPrec<double,neq>(pcdata, dom);
+  
+  return _pc;
+  
+}
+
+//------------------------------------------------------------------------------
+
+template<int dim>
+template<int neq, class MatVecProdOp>
+KspSolver<DistEmbeddedVec<double,neq>, MatVecProdOp, KspPrec<neq>, Communicator> *
+ImplicitEmbeddedTsDesc<dim>::createKrylovSolver(
+                               const DistInfo &info, KspData &kspdata,
+                               MatVecProdOp *_mvp, KspPrec<neq> *_pc,
+                               Communicator *_com)
+{
+  
+  KspSolver<DistEmbeddedVec<double,neq>, MatVecProdOp, KspPrec<neq>, Communicator> *_ksp = 0;
+  
+  if (kspdata.type == KspData::RICHARDSON)
+    _ksp = new RichardsonSolver<DistEmbeddedVec<double,neq>, MatVecProdOp,
+                 KspPrec<neq>, Communicator>(info, kspdata, _mvp, _pc, _com);
+  else if (kspdata.type == KspData::CG)
+    _ksp = new CgSolver<DistEmbeddedVec<double,neq>, MatVecProdOp,
+                 KspPrec<neq>, Communicator>(info, kspdata, _mvp, _pc, _com);
+  else if (kspdata.type == KspData::GMRES)
+    _ksp = new GmresSolver<DistEmbeddedVec<double,neq>, MatVecProdOp,
+                 KspPrec<neq>, Communicator>(info, kspdata, _mvp, _pc, _com);
+  
+  return _ksp;
+  
+}
+
+template<int dim>
+int ImplicitEmbeddedTsDesc<dim>::commonPart(DistSVec<double,dim> &U)
+{
+  // Adam 04/06/10: Took everything in common in solveNLAllFE and solveNLAllRK2 and put it here. Added Ghost-Points treatment for viscous flows.
+
+  //if(this->emmh && !this->inSubCycling) {  //subcycling is allowed from now on
+  if(this->emmh) {
+    int failSafe=0;
+    if(TsDesc<dim>::failSafeFlag == false){
+      *this->WstarijCopy = *this->Wstarij;
+      *this->WstarjiCopy = *this->Wstarji;
+      if(this->timeState->useNm1()) {
+        *this->Wstarij_nm1Copy = *this->Wstarij_nm1;
+        *this->Wstarji_nm1Copy = *this->Wstarji_nm1;
+      }
+      *this->nodeTagCopy = this->distLSS->getStatus();
+      *EmbeddedTsDesc<dim>::UCopy = U;
+    }
+    else{
+      *this->Wstarij = *this->WstarijCopy;
+      *this->Wstarji = *this->WstarjiCopy;
+      if(this->timeState->useNm1()) {
+        *this->Wstarij_nm1 = *this->Wstarij_nm1Copy;
+        *this->Wstarji_nm1 = *this->Wstarji_nm1Copy;
+      }
+      this->distLSS->setStatus(*this->nodeTagCopy);
+      this->nodeTag = *this->nodeTagCopy;
+    }
+
+    //get structure timestep dts
+    this->dts = this->emmh->update(0, 0, 0, this->bcData->getVelocityVector(), *this->Xs);
+
+
+    //recompute intersections
+    double tw = this->timer->getTime();
+
+    failSafe = this->distLSS->recompute(this->dtf, this->dtfLeft, this->dts,true,TsDesc<dim>::failSafeFlag); 
+    this->com->globalMin(1, &failSafe);
+    if(failSafe<0) //in case of intersection failure -1 is returned by recompute 
+       return failSafe;
+
+    this->timer->addIntersectionTime(tw);
+    this->com->barrier();
+    this->timer->removeIntersAndPhaseChange(tw);
+    if(this->riemannNormal==2)
+      this->spaceOp->computeCellAveragedStructNormal(*(this->Nsbar), this->distLSS);
+
+    //update nodeTags (only for numFluid>1)
+ //   if(this->numFluid>1) {
+      this->nodeTag0 = this->nodeTag;
+      this->nodeTag = this->distLSS->getStatus();
+ //   }
+
+    //store previous states for phase-change update
+    tw = this->timer->getTime();
+    this->spaceOp->updateSweptNodes(*this->X, this->phaseChangeChoice, this->phaseChangeAlg, U, this->Vtemp,
+            *this->Weights, *this->VWeights, *this->Wstarij, *this->Wstarji,
+            this->distLSS, (double*)this->vfar, (this->numFluid == 1 ? (DistVec<int>*)0 : &this->nodeTag));
+    this->timer->addEmbedPhaseChangeTime(tw);
+    this->timer->removeIntersAndPhaseChange(tw);
+
+    //this->timeState->update(U); 
+    this->timeState->getUn() = U;
+
+    // BDF update (Unm1)
+    if (this->timeState->useNm1() && this->timeState->existsNm1()) {
+      tw = this->timer->getTime();
+      DistSVec<double,dim>& Unm1 = this->timeState->getUnm1();
+  
+      if (!this->existsWstarnm1) {
+        
+        this->spaceOp->computeResidual(*this->X, *this->A, Unm1, *this->Wstarij, *this->Wstarji, this->distLSS,
+                                 this->linRecAtInterface, this->viscSecOrder, this->nodeTag, this->Vtemp, this->riemann, 
+                                 this->riemannNormal, this->Nsbar, 1, this->ghostPoints);
+      }
+
+      tw = this->timer->getTime();
+      this->spaceOp->updateSweptNodes(*this->X, this->phaseChangeChoice, this->phaseChangeAlg, Unm1, this->Vtemp,
+              *this->Weights, *this->VWeights, *this->Wstarij_nm1, *this->Wstarji_nm1,
+              this->distLSS, (double*)this->vfar, (this->numFluid == 1 ? (DistVec<int>*)0 : &this->nodeTag));
+      this->timer->addEmbedPhaseChangeTime(tw);
+      this->timer->removeIntersAndPhaseChange(tw);
+
+      this->timer->addEmbedPhaseChangeTime(tw);
+      this->timer->removeIntersAndPhaseChange(tw);
+    }
+
+    if (this->timeState->useNm1()) {
+      *this->Wstarij_nm1 = *this->Wstarij;
+      *this->Wstarji_nm1 = *this->Wstarji;
+      this->existsWstarnm1 = true;
+    }
+  
+  }
+
+//  // Ghost-Points Population
+//  if(this->eqsType == EmbeddedTsDesc<dim>::NAVIER_STOKES)
+//    {
+//      this->ghostPoints->deletePointers();
+//      this->spaceOp->populateGhostPoints(this->ghostPoints,*this->X,U,this->varFcn,this->distLSS,this->viscSecOrder,this->nodeTag);
+//      embeddedU.real() = U;
+//      embeddedU.setGhost(*this->ghostPoints,this->varFcn); 
+//    }
+  return 0;
+}
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+// External routine to solve problem (called by TsSolver)
+// It calls for the NewtonSolver ns, which in turn will
+// call routines below from this same file or from LevelSetTsDesc
+//------------------------------------------------------------------------------
+template<int dim>
+int ImplicitEmbeddedTsDesc<dim>::solveNonLinearSystem(DistSVec<double,dim> &U, int)
+{ 
+  double t0 = this->timer->getTime();
+  DistSVec<double,dim> Ubc(this->getVecInfo());
+  Ubc = U;
+
+  int its = 0;
+  its = commonPart(U); //failure gives negative values 
+  if(its<0)  //failSafe
+    return its;
+
+  TsDesc<dim>::setFailSafe(false);
+
+  its = this->ns->solve(U);
+  if(its==-10){
+    U=Ubc;
+    return its;
+  }
+  if(its<0){  //failSafe
+    U = *EmbeddedTsDesc<dim>::UCopy;
+    return its;
+  }
+  
+  this->timer->addFluidSolutionTime(t0);
+   
+  int ierr = this->checkSolution(U);
+  if(ierr>0){  //failSafe
+    U = *EmbeddedTsDesc<dim>::UCopy;
+    return (-ierr);
+  }
+
+  if(TsDesc<dim>::timeState->getData().typeIntegrator == ImplicitData::THREE_POINT_BDF &&
+                 TsDesc<dim>::timeStepCalculation == TsData::ERRORESTIMATION )
+    doErrorEstimation(U);
+
+  return its;
+}
+//------------------------------------------------------------------------------
+// External routines to solve Euler equations implicitly (called by NewtonSolver)
+//------------------------------------------------------------------------------
+
+// this function evaluates (Aw),t + F(w,x,v)
+template<int dim>
+void ImplicitEmbeddedTsDesc<dim>::computeFunction(int it, DistSVec<double,dim> &Q,
+                                                  DistSVec<double,dim> &F)
+{
+  //Kevin's debug
+//  int BuggyNode = 340680;
+  // phi is obtained once and for all for this iteration
+  // no need to recompute it before computation of jacobian.
+//  if (this->ghostPoints) {
+//    embeddedU.getGhost(*this->ghostPoints,this->varFcn); 
+//  }
+  this->spaceOp->computeResidual(*this->X, *this->A, Q, *this->Wstarij, *this->Wstarji, this->distLSS,
+                                 this->linRecAtInterface, this->viscSecOrder, this->nodeTag, F, this->riemann, 
+                                 this->riemannNormal, this->Nsbar, 1, this->ghostPoints);
+
+//  this->printNodalDebug(BuggyNode,-100,&F,&(this->nodeTag),&(this->nodeTag0));
+  this->timeState->add_dAW_dt(it, *this->geoState, *this->A, Q, F,this->distLSS);
+  this->spaceOp->applyBCsToResidual(Q, F,this->distLSS);
+}
+
+//------------------------------------------------------------------------------
+
+template<int dim>
+void ImplicitEmbeddedTsDesc<dim>::doErrorEstimation(DistSVec<double,dim> &U) 
+{
+  DistSVec<double,dim> *flux = new DistSVec<double,dim>(TsDesc<dim>::domain->getNodeDistInfo());
+
+  this->spaceOp->computeResidual(*this->X, *this->A, this->timeState->getUn(), *this->Wstarij, *this->Wstarji, this->distLSS,
+                                 this->linRecAtInterface, this->viscSecOrder, this->nodeTag, *flux, this->riemann, 
+                                 this->riemannNormal, this->Nsbar, 1, this->ghostPoints);
+
+  this->timeState->calculateErrorEstiNorm(U, *flux); 
+
+  delete flux;
+}
+
+//------------------------------------------------------------------------------
+
+template<int dim>
+void ImplicitEmbeddedTsDesc<dim>::recomputeFunction(DistSVec<double,dim> &Q,
+                                            DistSVec<double,dim> &rhs)
+{
+  this->spaceOp->recomputeRHS(*this->X, Q, rhs);
+}
+
+//------------------------------------------------------------------------------
+
+template<int dim>
+int ImplicitEmbeddedTsDesc<dim>::checkFailSafe(DistSVec<double,dim>& U)
+{
+//  this->com->fprintf(stdout, "WARNING: At the moment CheckFailSafe is not supported by the embedded framework with an implicit time-integrator!\n");
+
+  if (!this->failSafeNewton) return 0;
+
+  if (!this->tag)
+    this->tag = new DistSVec<bool,2>(this->getVecInfo());
+
+  this->domain->checkFailSafe(this->varFcn, U, *this->tag);
+  this->spaceOp->fix(*this->tag);
+
+  return 1;
+
+}
+
+//------------------------------------------------------------------------------
+template<int dim> 
+void ImplicitEmbeddedTsDesc<dim>::resetFixesTag()
+{
+
+  this->spaceOp->resetTag();
+
+}
+
+//------------------------------------------------------------------------------
---
+++ MultiGridSegTsDesc.C
@@ -0,0 +1,250 @@
+#include <VectorSet.h>
+#include <LevelSet/LevelSetStructure.h>
+#include <MultiGridSegTsDesc.h>
+
+template <int dim,int neq1,int neq2>
+MultiGridSegTsDesc<dim,neq1,neq2>::
+MultiGridSegTsDesc(IoData & iod, GeoSource & gs,  Domain * dom) :
+  ImplicitSegTsDesc<dim,neq1,neq2>(iod,gs,dom) {
+
+  memset(numSmooths_post,0,sizeof(numSmooths_post));
+  numSmooths_pre[0] = 1;
+  numSmooths_pre[1] = 2;
+  numSmooths_pre[2] = 3;
+  numSmooths_pre[3] = 3;
+  numSmooths_pre[4] = 3;
+  numSmooths_pre[5] = 3;
+
+ 
+  prolong_relax_factor = iod.mg.prolong_relax_factor;
+  restrict_relax_factor = iod.mg.restrict_relax_factor;
+
+  if (iod.mg.cycle_scheme == MultiGridData::VCYCLE)
+    mc = 1;
+  else if (iod.mg.cycle_scheme == MultiGridData::WCYCLE)
+    mc = 2;     
+
+  smoothWithGMRES = (iod.mg.mg_smoother == MultiGridData::MGGMRES);
+
+  globalIt = 0;
+}
+
+template <int dim,int neq1,int neq2>
+MultiGridSegTsDesc<dim,neq1,neq2>::
+~MultiGridSegTsDesc() {
+
+}
+
+
+template <int dim,int neq1,int neq2>
+void MultiGridSegTsDesc<dim,neq1,neq2>::
+setupTimeStepping(DistSVec<double,dim> *U0, IoData &iod) {
+
+  ImplicitSegTsDesc<dim,neq1,neq2>::setupTimeStepping(U0,iod);
+
+  pKernel = new MultiGridKernel<double>(this->domain, *this->geoState,
+                                        iod,iod.mg.num_multigrid_levels);
+
+  pKernel->initialize(dim,neq1,neq2);
+
+  mgSpaceOp = 
+    new MultiGridSpaceOperator<double,dim>(iod, this->domain, this->spaceOp, pKernel,
+                                           this->spaceOp1);
+
+  mgMvp1 = new MultiGridMvpMatrix<double,neq1>(this->domain,pKernel);
+
+  mgKspSolver = new MultiGridKspSolver<double,neq1,double>(this->domain, iod.ts.implicit.newton.ksp.ns,
+                                                          pKernel);
+
+  pKernel->setUseVolumeWeightedAverage(iod.mg.restrictMethod == MultiGridData::VOLUME_WEIGHTED);
+
+  if (!smoothWithGMRES) {
+    typename MultiGridSmoothingMatrix<double,neq1>::SmoothingMode s;
+    if (iod.mg.mg_smoother == MultiGridData::MGRAS)
+      s = MultiGridSmoothingMatrix<double,neq1>::RAS; 
+    if (iod.mg.mg_smoother == MultiGridData::MGJACOBI)
+      s = MultiGridSmoothingMatrix<double,neq1>::BlockJacobi;
+    if (iod.mg.mg_smoother == MultiGridData::MGLINEJACOBI)
+      s = MultiGridSmoothingMatrix<double,neq1>::LineJacobi;
+    smoothingMatrices1 = new MultiGridSmoothingMatrices<double,neq1>(pKernel, s);
+    
+    typename MultiGridSmoothingMatrix<double,neq2>::SmoothingMode s2;
+    if (iod.mg.mg_smoother == MultiGridData::MGRAS)
+      s2 = MultiGridSmoothingMatrix<double,neq2>::RAS; 
+    if (iod.mg.mg_smoother == MultiGridData::MGJACOBI)
+      s2 = MultiGridSmoothingMatrix<double,neq2>::BlockJacobi;
+    if (iod.mg.mg_smoother == MultiGridData::MGLINEJACOBI)
+      s2 = MultiGridSmoothingMatrix<double,neq2>::LineJacobi;
+    smoothingMatrices2 = new MultiGridSmoothingMatrices<double,neq2>(pKernel,s2);
+
+  }
+
+  V.init(pKernel);
+  res.init(pKernel);
+  R.init(pKernel);
+  R1.init(pKernel);
+  R2.init(pKernel);
+  F.init(pKernel);
+  Forig.init(pKernel);
+  U.init(pKernel);
+  Uold.init(pKernel); 
+  dx.init(pKernel);
+  dx1.init(pKernel);
+  dx2.init(pKernel);
+  
+}
+
+template <int dim,int neq1,int neq2>
+void MultiGridSegTsDesc<dim,neq1,neq2>::
+smooth0(DistSVec<double,dim>& x,int steps) {
+
+  int i;
+  double dummy = 0.0;
+  this->updateStateVectors(x, 0);
+  for (i = 0; i < steps; ++i) {
+
+    this->computeTimeStep(globalIt,&dummy, x);
+    ++globalIt;
+    this->computeFunction(0, x, R(0));
+    this->computeJacobian(0, x, R(0));
+    if (smoothWithGMRES)
+      this->setOperators(x);
+    else {
+      smoothingMatrices1->acquire(*this->GetJacobian1());
+      smoothingMatrices2->acquire(*this->GetJacobian2());
+    }
+  
+    R(0) *= -1.0;
+    if (smoothWithGMRES)
+      this->solveLinearSystem(0, R(0),dx(0));
+    else {
+      R(0).split(R1(0), R2(0));
+      smoothingMatrices1->apply(0, dx1, R1);
+      smoothingMatrices2->apply(0, dx2, R2);
+      dx(0).merge(dx1(0),dx2(0));
+    }
+    
+    x += dx(0);
+    
+    this->updateStateVectors(x, 0);
+    this->monitorConvergence(0, x);
+    R(0) = -1.0*this->getCurrentResidual();
+    
+  }
+  if (i == 0) {
+    this->monitorConvergence(0, x);
+    R(0) = -1.0*this->getCurrentResidual();
+  }
+  double one = 1.0;
+  if (globalIt%500 == 1) 
+    this->domain->writeVectorToFile("myResidual", globalIt/500, globalIt, R(0), &one);
+}
+
+template <int dim,int neq1,int neq2>
+void MultiGridSegTsDesc<dim,neq1,neq2>::
+smooth(int lvl, MultiGridDistSVec<double,dim>& x,
+       DistSVec<double,dim>& f,int steps) {
+
+  int i;
+  for (i = 0; i < steps; ++i) {
+
+    this->varFcn->conservativeToPrimitive(x(lvl), V(lvl));
+
+    mgSpaceOp->updateStateVectors(lvl,x);
+
+    mgSpaceOp->computeTimeStep(lvl,this->data->cfl*pow(0.75,lvl),
+                               V);
+ 
+    mgSpaceOp->computeResidual(lvl, x, V, res);
+    mgSpaceOp->computeJacobian(lvl, x, V, *mgMvp1);
+    R(lvl) = f-1.0*res(lvl);
+    R(lvl).split(R1(lvl), R2(lvl));
+    if (smoothWithGMRES) {
+      mgKspSolver->solve(lvl, *mgMvp1, R1, dx1);
+    }
+    else {
+      smoothingMatrices1->acquire(lvl, *mgMvp1);
+      smoothingMatrices1->apply(lvl, dx1, R1); 
+    }
+    dx2(lvl) = 0.0;
+    dx(lvl).merge(dx1(lvl), dx2(lvl));
+    
+    x(lvl) += dx(lvl);
+   
+    this->varFcn->conservativeToPrimitive(x(lvl), V(lvl));
+
+    pKernel->fixNegativeValues(lvl,V(lvl), x(lvl), dx(lvl), f,Forig(lvl), this->varFcn,
+                               mgSpaceOp->getOperator(lvl));
+
+  }
+ 
+  if (steps > 0) {   
+    mgSpaceOp->computeResidual(lvl, x, V, R, false);
+    R(lvl) = f-R(lvl);
+  }
+/*
+  if (lvl == 1) {
+    pKernel->getLevel(lvl)->writePVTUSolutionFile("r.sol",x(lvl));
+    MPI_Barrier(MPI_COMM_WORLD);
+    exit(0);
+  }
+*/
+}
+
+template <int dim,int neq1,int neq2>
+void MultiGridSegTsDesc<dim,neq1,neq2>::cycle(int lvl, DistSVec<double,dim>& f,
+                                    MultiGridDistSVec<double,dim>& x) {
+
+  if (lvl == 0) { 
+    smooth0(x(lvl), numSmooths_pre[0]);
+    mgSpaceOp->setupBcs(this->getSpaceOperator()->getDistBcData());
+  }
+  else
+    smooth(lvl,x, f,  numSmooths_pre[lvl]);
+
+  if (lvl < pKernel->numLevels()-1) {
+
+    pKernel->Restrict(lvl+1, x(lvl), U(lvl+1));
+    //this->domain->getCommunicator()->fprintf(stderr,"Restricting residual...\n");
+    //fflush(stderr);
+    //MPI_Barrier(MPI_COMM_WORLD);
+    pKernel->Restrict(lvl+1, R(lvl), R(lvl+1));
+    Uold(lvl+1) = U(lvl+1);
+    
+    this->varFcn->conservativeToPrimitive(U(lvl+1), V(lvl+1));
+    mgSpaceOp->computeResidual(lvl+1, U, V, F, false);
+    pKernel->applyFixes(lvl+1, R(lvl+1));
+    //pKernel->fixNegativeValues(lvl+1,V(lvl+1), U(lvl+1), dx(lvl+1), F(lvl+1), this->varFcn);
+    /*if (lvl == 0 && globalIt % 25 == 0) {
+
+      pKernel->getLevel(lvl+1)->writePVTUSolutionFile("myR",R(lvl+1));
+    }*/
+    Forig(lvl+1) = F(lvl+1);
+    F(lvl+1) += R(lvl+1)*restrict_relax_factor;
+    for (int i = 0; i < mc; ++i)
+      cycle(lvl+1, F(lvl+1), U);
+    
+    pKernel->Prolong(lvl+1, Uold(lvl+1), U(lvl+1), x(lvl), prolong_relax_factor);
+  }
+  
+  if (lvl == 0) 
+    smooth0(x(lvl), numSmooths_post[0]);
+  else
+    smooth(lvl,x, f,  numSmooths_post[lvl]);
+}
+
+template <int dim,int neq1,int neq2>
+void MultiGridSegTsDesc<dim,neq1,neq2>::cycle(DistSVec<double,dim>& x) {
+
+  F(0) = 0.0;
+
+  U(0) = x;
+  
+  cycle(0, F(0), U);
+
+  x = U(0); 
+}
+
+template class MultiGridSegTsDesc<6,5,1>;
+template class MultiGridSegTsDesc<7,5,2>;
+
