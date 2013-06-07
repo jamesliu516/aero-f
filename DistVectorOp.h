3aeec6a23f4cae4ddfb82226d365cc02f6e89e04
Alex Main <alexmain@stanford.edu>
2013-06-07 15:14:19 -0700

    Updated rapid pressure/density change flags

--- DistVectorOp.h
+++ DistVectorOp.h
@@ -51,4 +51,25 @@ public:
   }
 
 
+  template <class Scalar1, int dim1,
+            class Scalar2,int dim2,
+            class Scalar3,class Operation>
+  static void Op(DistSVec<Scalar1,dim1>& v1,DistSVec<Scalar2,dim2>& v2,
+                DistVec<Scalar3>& v3, const Operation& oper) {
+
+    const DistInfo& distInfo = v1.info();
+
+#pragma omp parallel for
+    for (int iSub = 0; iSub < distInfo.numLocThreads; ++iSub) {
+
+      int locOffset = distInfo.subOffsetReg[iSub];
+      int locLen = distInfo.subLenReg[iSub];
+
+      for (int i = 0; i < locLen; ++i) 
+      //  this->v[locOffset+i] = v2.v[locOffset+i];
+        oper.Perform(v1.v[locOffset+i],v2.v[locOffset+i],
+                     v3.v[locOffset+i]);
+    }
+  }
+
 };
