59d8cc29ed4f5ce0066500397932b8ea28022464
Alex Main <alexmain@stanford.edu>
2013-06-08 12:28:24 -0700

    Fixed bug & compile error

--- EdgeGrad.h
+++ EdgeGrad.h
@@ -12,14 +12,7 @@ template<class Scalar> class Vec;
 
 //------------------------------------------------------------------------------
 
-struct V6NodeData {
-  int tet;
-  int face;
-  double r;
-  double t;
-  V6NodeData() { tet = -1; face = -1; r = 0.0; t = 0.0; }
-};
-
+#include "V6NodeData.h"
 //------------------------------------------------------------------------------
 
 template<int dim>
