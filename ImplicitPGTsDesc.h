99cd7d92ada870c60aa845c49b2e24f0883bd62e
Kyle Washabaugh <kwash@stanford.edu>
2015-07-23 16:10:04 -0700

    Fixed a few bugs in Nonlinear ROM preprocessing.

---
+++ ImplicitPGTsDesc.h
@@ -0,0 +1,76 @@
+#ifndef _IMPLICIT_PG_TS_DESC_H_
+#define _IMPLICIT_PG_TS_DESC_H_
+
+#include <ImplicitRomTsDesc.h>
+#include <ParallelRom.h>
+#include <RefVector.h>
+#include <VectorSet.h>
+#include <DistVector.h>
+//------------------------------------------------------------------------------
+
+template<int dim>
+class ImplicitPGTsDesc : public ImplicitRomTsDesc<dim> {
+
+private:
+  double **lsCoeff;
+  RefVec<DistSVec<double, dim> >residualRef;
+  int currentProblemSize; // for local rom  
+
+protected:
+
+  ParallelRom<dim> *parallelRom;
+  Vec<double> rhs;
+  Vec<double> From;
+  void saveNewtonSystemVectors(const int totalTimeSteps) {this->saveNewtonSystemVectorsAction(totalTimeSteps);}
+  void solveNewtonSystem(const int &, double &, bool &, DistSVec<double, dim> &, const int& totalTimeSteps = 0);
+	int lsSolver;
+	double *jactmp;
+  KspPrec<dim> *pc;
+
+  DistSVec<double, dim>* A_Uinlet;
+  Vec<double>* PhiT_A_Uinlet;
+  Vec<double>* PhiT_A_U;
+  VecSet< Vec<double> >* PhiT_A_Phi;
+  VecSet<DistSVec<double, dim> >* A_Phi;
+  double regWeightConstant;
+  double Kc;
+  double regWeightProportional;
+  double Kp;
+  double regWeightIntegral;
+  double Ki;
+  double Ki_leak;
+  double dKi;
+  double dRegWeightIntegral;
+  double ffError;
+  double ffErrorPrev;
+  double ffErrorTol;
+  double minRes;
+
+  double rhsNormInit;
+
+  void setProblemSize(DistSVec<double, dim> &);
+
+  double computePGResidualNorm(DistSVec<double,dim> &);
+  void setReferenceResidual();
+
+  int controlNodeGlobalID;
+  int controlNodeLocalID;
+  int controlNodeSubDomain;
+  int controlNodeCpuNum;
+
+public:
+  bool checkForLastIteration(IoData &, int, double, double, DistSVec<double,dim> &);
+  void monitorInitialState(int, DistSVec<double,dim> &);
+  bool monitorConvergence(int, DistSVec<double,dim> &);
+  
+  ImplicitPGTsDesc(IoData &, GeoSource &, Domain *);
+  ~ImplicitPGTsDesc();
+};
+
+//------------------------------------------------------------------------------
+
+#ifdef TEMPLATE_FIX
+#include <ImplicitPGTsDesc.C>
+#endif
+
+#endif
---
+++ Solvers/NavierStokesEmbeddedCoupledSolver.h
@@ -0,0 +1,50 @@
+#ifndef _NAVIER_STOKES_EMBEDDED_COUPLEDSOLVER_H_
+#define _NAVIER_STOKES_EMBEDDED_COUPLEDSOLVER_H_
+#include <IoData.h>
+#include <GeoSource.h>
+#include <Domain.h>
+#include <TsSolver.h>
+#include <ExplicitEmbeddedTsDesc.h>
+#include <MultiGridEmbeddedTsDesc.h>
+#include <ImplicitEmbeddedCoupledTsDesc.h>
+#include <MultiGridSolver.h>
+#include <EmbeddedFluidShapeOptimizationHandler.h>
+
+template<int dim>
+void startNavierStokesEmbeddedCoupledSolver(IoData &ioData, GeoSource &geoSource, Domain &domain)
+{
+
+  Communicator *com = domain.getCommunicator();
+
+  domain.createVecPat(dim, &ioData);
+  domain.createRhsPat(dim, ioData);
+
+  if (ioData.problem.alltype == ProblemData::_SHAPE_OPTIMIZATION_) {
+    EmbeddedFluidShapeOptimizationHandler<dim> fsoh(ioData, geoSource, &domain);
+    TsSolver<EmbeddedFluidShapeOptimizationHandler<dim> > tsSolver(&fsoh);
+    tsSolver.fsoSolve(ioData);
+  }
+
+  else if (ioData.problem.solutionMethod == ProblemData::TIMESTEPPING) {
+    
+    if (ioData.ts.type == TsData::IMPLICIT) {
+      
+      ImplicitEmbeddedCoupledTsDesc<dim> tsDesc(ioData, geoSource, &domain);
+      TsSolver<ImplicitEmbeddedCoupledTsDesc<dim> > tsSolver(&tsDesc);
+      tsSolver.solve(ioData);
+    }
+    else{
+      ExplicitEmbeddedTsDesc<dim> tsDesc(ioData, geoSource, &domain);
+      TsSolver<ExplicitEmbeddedTsDesc<dim> > tsSolver(&tsDesc);
+      tsSolver.solve(ioData);
+    }
+  } else {
+
+    MultiGridEmbeddedTsDesc<dim> tsDesc(ioData, geoSource, &domain);
+    MultiGridSolver<MultiGridEmbeddedTsDesc<dim> > mgSolver(&tsDesc);
+    mgSolver.solve(ioData);
+  }
+
+}
+
+#endif
