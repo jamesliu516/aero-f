06083b09a5c21d8f573f910a71ea0085c9e492ab
Philip Avery <pavery@stanford.edu>
2016-10-11 09:57:40 -0400

    Fix compiler issues with gcc 6.2

--- ImplicitGappyTsDesc.h
+++ ImplicitGappyTsDesc.h
@@ -16,8 +16,13 @@ class ImplicitGappyTsDesc : public ImplicitRomTsDesc<dim> {
 
 protected:
 
+#if (__cplusplus >= 201103L)
+  std::unique_ptr< VecSet < DistSVec<double, dim> > > AJRestrict;
+  std::unique_ptr< DistSVec<double, dim> > ResRestrict;
+#else
   std::auto_ptr< VecSet < DistSVec<double, dim> > > AJRestrict;
   std::auto_ptr< DistSVec<double, dim> > ResRestrict;
+#endif
 
   void setProblemSize(DistSVec<double, dim> &) = 0;
 
